559bd07faac1651ee89f21ef497e54a0787f42c2
jcheron <myaddressmail@gmail.com>
2016-09-27 20:54:08 +0200

    config

--- project-files/app/config.php
+++ project-files/app/config.php
@@ -3,6 +3,7 @@ return array(
 		"siteUrl"=>"%siteUrl%",
 		"documentRoot"=>"%documentRoot%",
 		"database"=>[
+				"dbName"=>"%dbName%",
 				"serverName"=>"127.0.0.1",
 				"port"=>"3306",
 				"user"=>"root",
