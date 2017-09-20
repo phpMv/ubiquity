3a592dde576124a2e3f7ca2088e84d35de37231c
jcheron <myaddressmail@gmail.com>
2017-09-20 19:24:16 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'micro-framework'
+project = 'Ubiquity framework'
 copyright = '2017, phpmv'
 author = 'phpmv'
 
