ebad564e645405802a97d283896a3f2397520ef6
jcheron <myaddressmail@gmail.com>
2017-10-18 20:27:47 +0200

    doc conf pygment style

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,6 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'vim'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -84,6 +83,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+pygments_style = 'vim'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
