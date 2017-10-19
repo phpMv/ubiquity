b9f611cfd7b61f5fc9adde07429c209cd806c3cd
jcheron <myaddressmail@gmail.com>
2017-10-19 19:51:13 +0200

    doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,8 @@ html_theme = 'sphinx_rtd_theme'
 #
 # html_theme_options = {}
 
+html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'], }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
