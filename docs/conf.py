0bf2bb0194f59aedaed3cd921001f47d22471568
jcheron <myaddressmail@gmail.com>
2017-10-19 19:56:29 +0200

    doc toc depth 1

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'navigation_depth': 4,}
+html_theme_options = {'navigation_depth': 1,}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
