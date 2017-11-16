96a1e3388939f3e46b175b14db662d62286eccb2
jcheron <myaddressmail@gmail.com>
2017-11-16 20:02:30 +0100

    sl update

--- micro/orm/reverse/Table.php
+++ micro/orm/reverse/Table.php
@@ -7,17 +7,15 @@ use micro\orm\OrmUtils;
 class Table {
 	private $model;
 
-	public function __construct($model){
+	public function __construct($model) {
 		$this->model=$model;
 	}
 
-	public function generateSQL(){
+	public function generateSQL() {
 		$metas=OrmUtils::getModelMetadata($this->model);
 		$table=OrmUtils::getTableName($this->model);
 		$fieldnames=OrmUtils::getAnnotationInfo($this->model, "#fieldNames");
 		$primaryKeys=OrmUtils::getKeyFields($this->model);
 		$serializables=OrmUtils::getSerializableFields($this->model);
-		\var_dump($serializables);
-		\var_dump(OrmUtils::getFieldNames($this->model));
 	}
 }
