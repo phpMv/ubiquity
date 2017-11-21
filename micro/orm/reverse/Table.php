0feaa6aa85462d83d2191537320a34961a2f0ef4
jcheron <myaddressmail@gmail.com>
2017-11-21 02:11:12 +0100

    Table !

--- micro/orm/reverse/Table.php
+++ micro/orm/reverse/Table.php
@@ -7,11 +7,11 @@ use micro\orm\OrmUtils;
 class Table {
 	private $model;
 
-	public function __construct($model) {
+	public function __construct($model){
 		$this->model=$model;
 	}
 
-	public function generateSQL() {
+	public function generateSQL(){
 		$metas=OrmUtils::getModelMetadata($this->model);
 		$table=OrmUtils::getTableName($this->model);
 		$fieldnames=OrmUtils::getAnnotationInfo($this->model, "#fieldNames");
