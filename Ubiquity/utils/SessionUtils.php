30ad8f755e5466cc8c662338fba4d10c4f74cd29
jcheron <myaddressmail@gmail.com>
2018-02-12 03:23:21 +0100

    Session methods added

--- Ubiquity/utils/SessionUtils.php
+++ Ubiquity/utils/SessionUtils.php
@@ -3,9 +3,9 @@
 namespace Ubiquity\utils;
 
 /**
- * Utilitaires liés à la session
+ * Session utilities
  * @author jc
- * @version 1.0.0.1
+ * @version 1.0.0.2
  */
 class SessionUtils {
 
@@ -15,7 +15,7 @@ class SessionUtils {
 	 * @return array
 	 */
 	public static function getArray($arrayKey) {
-		if (array_key_exists($arrayKey, $_SESSION)) {
+		if (isset($_SESSION[$arrayKey])) {
 			$array=$_SESSION[$arrayKey];
 			if (!is_array($array))
 				$array=array ();
@@ -43,23 +43,55 @@ class SessionUtils {
 	}
 
 	public static function checkBoolean($key) {
-		$_SESSION[$key]=!SessionUtils::getBoolean($key);
+		$_SESSION[$key]=!self::getBoolean($key);
 		return $_SESSION[$key];
 	}
 
 	public static function getBoolean($key) {
 		$ret=false;
-		if (array_key_exists($key, $_SESSION)) {
+		if (isset($_SESSION[$key])) {
 			$ret=$_SESSION[$key];
 		}
 		return $ret;
 	}
 
 	public static function session($key, $default=NULL) {
-		return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+		return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
+	}
+
+	public static function get($key, $default=NULL) {
+		return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
+	}
+
+	public static function set($key, $value) {
+		$_SESSION[$key]=$value;
 	}
 
 	public static function delete($key) {
 		unset($_SESSION[$key]);
+	}
+
+	public static function start($name=null){
+		if(isset($name)) \session_name();
+		if (!isset($_SESSION)) { \session_start(); }
+	}
+
+	public static function isStarted(){
+		return isset($_SESSION);
+	}
+
+	public static function terminate(){
+		if(!self::isStarted())
+			return;
+		$_SESSION = array();
+
+		if (\ini_get("session.use_cookies")) {
+			$params = \session_get_cookie_params();
+			\setcookie(\session_name(), '', \time() - 42000,
+					$params["path"], $params["domain"],
+					$params["secure"], $params["httponly"]
+					);
+		}
+		\session_destroy();
 	}
 }
