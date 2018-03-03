5287e9a796a96c7b52171c0290649bf6077fac22
jcheron <myaddressmail@gmail.com>
2018-03-03 15:11:26 +0100

    postInstall index semantic

---
+++ Ubiquity/core/postinstall/Display.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace core\postinstall;
+
+use Ubiquity\core\Framework;
+
+class Display {
+
+	public static function semanticMenu($id, $semantic) {
+		$menu=$semantic->htmlMenu($id, [ "Ubiquity website","Guide","Doc API","GitHub" ]);
+		$menu->asLinks([ "https://ubiquity.kobject.net","https://micro-framework.readthedocs.io/en/latest/?badge=latest","https://api.kobject.net/ubiquity/","https://github.com/phpMv/ubiquity" ], 'new');
+		$menu->setSecondary();
+		if (Framework::hasAdmin()) {
+			$menu->addItem(new \Ajax\semantic\html\elements\html5\HtmlLink("", "Admin", "UbiquityMyAdmin", "admin"));
+		}
+		return $menu;
+	}
+}
+
