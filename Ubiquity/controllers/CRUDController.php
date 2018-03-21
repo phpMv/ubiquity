5659276c9b3923709c64b39ada8fd53842ade4dd
jcheron <myaddressmail@gmail.com>
2018-03-21 20:32:27 +0100

    Startup parameter ($controller) added

--- Ubiquity/controllers/CRUDController.php
+++ Ubiquity/controllers/CRUDController.php
@@ -2,11 +2,20 @@
 
 namespace Ubiquity\controllers;
 
+use Ubiquity\orm\DAO;
+
 abstract class CRUDController extends ControllerBase {
 	protected $model;
-	protected $views;
+	protected $gui;
 
+	abstract function getGui();
+
+	/**
+	 * Default page : list all objects
+	 */
 	public function index() {
+		$objects = DAO::getAll ( $this->model );
+		return $this->gui->renderObjects ( $objects );
 	}
 }
 
