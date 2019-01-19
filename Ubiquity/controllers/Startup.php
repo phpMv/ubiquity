57c2bb9a7d9eeb0216693cf7663cb041c75aa64a
jcheron <myaddressmail@gmail.com>
2019-01-19 18:42:23 +0100

    comment for route response stored in cache

--- Ubiquity/controllers/Startup.php
+++ Ubiquity/controllers/Startup.php
@@ -28,7 +28,11 @@ class Startup {
 	public static function forward($url,$initialize=true,$finalize=true) {
 		$u = self::parseUrl ( $url );
 		if (($ru = Router::getRoute ( $url )) !== false) {
+			if (\is_array ( $ru )){
 				self::runAction ( $ru ,$initialize,$finalize);
+			}else{
+				echo $ru;//Displays route response from cache
+			}
 		} else {
 			self::setCtrlNS ();
 			$u [0] = self::$ctrlNS . $u [0];
