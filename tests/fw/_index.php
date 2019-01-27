4fe269faf5bc536a0d1fb3ca3251e23ce3f0f6e6
jcheron <myaddressmail@gmail.com>
2019-01-27 15:30:58 +0100

    remove session

--- tests/fw/_index.php
+++ tests/fw/_index.php
@@ -6,6 +6,7 @@ include 'c3.php';
 define ( 'MY_APP_STARTED', true );
 $config = include ROOT . 'config/config.php';
 $config ["siteUrl"] = "http://local.dev/prj-test-admin/";
+$config ['sessionName'] = null;
 require ROOT . './../vendor/autoload.php';
 require ROOT . 'config/services.php';
 \Ubiquity\controllers\Router::get ( "c3/(.*?)", function () {
