2a510ceeac2c977f309bcad1848626278a2c87b7
jcheron <myaddressmail@gmail.com>
2019-01-27 14:47:29 +0100

    Update SigninCest.php

--- tests/acceptance/SigninCest.php
+++ tests/acceptance/SigninCest.php
@@ -2,11 +2,12 @@
 class SigninCest {
 
 	public function _before(AcceptanceTester $I) {
+		$I->amOnPage ( "/" );
 	}
 
 	// tests
 	public function tryToTest(AcceptanceTester $I) {
-		$I->amOnPage ( "/prj-test-admin/" );
+		$I->amOnPage ( "/" );
 		$I->seeElement ( 'body' );
 	}
 }
