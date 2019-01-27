ef6deb286b0d868da1c35cadb765e8ef1dbbfa3b
jcheron <myaddressmail@gmail.com>
2019-01-27 14:23:55 +0100

    local.dev host

--- tests/fw/_index.php
+++ tests/fw/_index.php
@@ -5,6 +5,7 @@ define ( 'ROOT', __DIR__ . DS . 'app' . DS );
 include 'c3.php';
 define ( 'MY_APP_STARTED', true );
 $config = include ROOT . 'config/config.php';
+$config ["siteUrl"] = "http://local.dev/prj-test-admin/";
 require ROOT . './../vendor/autoload.php';
 require ROOT . 'config/services.php';
 \Ubiquity\controllers\Router::get ( "c3/(.*?)", function () {
