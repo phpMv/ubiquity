3422976171ec24a4e5eb9d34fda695b819690559
jcheron <myaddressmail@gmail.com>
2019-01-30 00:57:44 +0100

    remote to true with acceptance.remote.coverage file

--- tests/acceptance/SigninCest.php
+++ tests/acceptance/SigninCest.php
@@ -10,26 +10,22 @@ class SigninCest {
 
 	// tests
 	public function tryToTest(AcceptanceTester $I) {
-		/*
-		 * $I->amOnPage ( "/" );
-		 * $I->seeElement ( 'body' );
-		 * $I->see ( 'Ubiquity', [ 'css' => 'body' ] );
-		 */
+		$I->amOnPage ( "/" );
+		$I->seeElement ( 'body' );
+		$I->see ( 'Ubiquity', [ 'css' => 'body' ] );
 	}
 
 	// tests
 	public function tryToGotoAdmin(AcceptanceTester $I) {
-		/*
-		 * $I->amOnPage ( "/Admin/index" );
-		 * $I->seeInCurrentUrl ( "Admin/index" );
-		 * $I->see ( 'Used to perform CRUD operations on data', [ 'css' => 'body' ] );
-		 * $I->click ( "a[href='Admin/Models']" );
-		 * $I->waitForElementVisible ( "#content-header", 10 );
-		 * $I->canSeeInCurrentUrl ( "/Admin/Models" );
-		 * $I->click ( "a[data-model='models.Connection']" );
-		 * $I->waitForElementVisible ( "#btAddNew", 10 );
-		 * $I->canSeeInCurrentUrl ( "/Admin/showModel/models.Connection" );
-		 * $I->see ( 'organizations/display/4', "#lv td" );
-		 */
+		$I->amOnPage ( "/Admin/index" );
+		$I->seeInCurrentUrl ( "Admin/index" );
+		$I->see ( 'Used to perform CRUD operations on data', [ 'css' => 'body' ] );
+		$I->click ( "a[href='Admin/Models']" );
+		$I->waitForElementVisible ( "#content-header", 10 );
+		$I->canSeeInCurrentUrl ( "/Admin/Models" );
+		$I->click ( "a[data-model='models.Connection']" );
+		$I->waitForElementVisible ( "#btAddNew", 10 );
+		$I->canSeeInCurrentUrl ( "/Admin/showModel/models.Connection" );
+		$I->see ( 'organizations/display/4', "#lv td" );
 	}
 }
