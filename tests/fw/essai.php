ce8c236e5cc05db924bbc914a2ce561d446bed32
jcheron <myaddressmail@gmail.com>
2019-02-01 04:17:56 +0100

    Update essai.php

--- tests/fw/essai.php
+++ tests/fw/essai.php
@@ -1,5 +1,6 @@
 <?php
 include 'Ubiquity/utils/base/UFileSystem.php';
+include 'Ubiquity/utils/base/traits/UFileSystemWriter.php';
 use Ubiquity\utils\base\UFileSystem;
 echo UFileSystem::load ( ROOT . 'config/services.php' );
 echo "*************************************************";
