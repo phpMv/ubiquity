0b50d94c31510c33770b81f8e251cd2bf94a5374
jcheron <myaddressmail@gmail.com>
2019-02-02 12:24:02 +0100

    travis phantomJs

--- src/tests/acceptance/AdminCest.php
+++ src/tests/acceptance/AdminCest.php
@@ -62,9 +62,9 @@ class AdminCest {
 		$I->canSee ( 'TestController', '#msgGlobal' );
 		$I->canSee ( 'controller has been created in', '#msgGlobal' );
 		$I->canSee ( 'The default view associated has been created in', '#msgGlobal' );
-		$I->wait ( 5 );
+		$I->wait ( 10 );
 		$I->click ( "#filter-bt" );
-		$I->waitForElementVisible ( "#filtering-frm" );
+		$I->waitForElementVisible ( "#filtering-frm", self::TIMEOUT );
 		$I->checkOption ( "[data-value='controllers\TestController']" );
 		$I->click ( "#validate-btn" );
 		// $I->canSeeElement ( "#bt-controllers5CTestController" ); // Controller button
