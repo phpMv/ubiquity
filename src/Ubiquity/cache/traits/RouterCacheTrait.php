76768f2013a6a61f284dc07507dc19f9307a8f95
jcheron <myaddressmail@gmail.com>
2019-02-02 13:41:43 +0100

    chromeDriver for Travis + remove -a on composer

--- src/Ubiquity/cache/traits/RouterCacheTrait.php
+++ src/Ubiquity/cache/traits/RouterCacheTrait.php
@@ -10,138 +10,140 @@ use Ubiquity\utils\base\UArray;
 use Ubiquity\cache\CacheManager;
 
 /**
+ *
  * @author jcheron <myaddressmail@gmail.com>
  * @version 1.0.3
  * @staticvar AbstractDataCache $cache
  *
  */
-trait RouterCacheTrait{
-	public static $minify=false;
-	
-	abstract protected static function _getFiles(&$config, $type, $silent=false);
-	private static $expiredRoutes=[ ];
+trait RouterCacheTrait {
+	public static $minify = false;
+
+	abstract protected static function _getFiles(&$config, $type, $silent = false);
+	private static $expiredRoutes = [ ];
 
 	private static function addControllerCache($classname) {
-		$parser=new ControllerParser();
+		$parser = new ControllerParser ();
 		try {
-			$parser->parse($classname);
-			return $parser->asArray(self::$minify);
+			$parser->parse ( $classname );
+			return $parser->asArray ( self::$minify );
 		} catch ( \Exception $e ) {
 			// Nothing to do
 		}
 		return [ ];
 	}
 
-	private static function initRouterCache(&$config, $silent=false) {
-		$routes=[ "rest" => [ ],"default" => [ ] ];
-		$files=self::getControllersFiles($config,$silent);
+	private static function initRouterCache(&$config, $silent = false) {
+		$routes = [ "rest" => [ ],"default" => [ ] ];
+		$files = self::getControllersFiles ( $config, $silent );
 		foreach ( $files as $file ) {
-			if (is_file($file)) {
-				$controller=ClassUtils::getClassFullNameFromFile($file);
-				$parser=new ControllerParser();
+			if (is_file ( $file )) {
+				$controller = ClassUtils::getClassFullNameFromFile ( $file );
+				$parser = new ControllerParser ();
 				try {
-					$parser->parse($controller);
-					$ret=$parser->asArray(self::$minify);
-					$key=($parser->isRest()) ? "rest" : "default";
-					$routes[$key]=\array_merge($routes[$key], $ret);
+					$parser->parse ( $controller );
+					$ret = $parser->asArray ( self::$minify );
+					$key = ($parser->isRest ()) ? "rest" : "default";
+					$routes [$key] = \array_merge ( $routes [$key], $ret );
 				} catch ( \Exception $e ) {
 					// Nothing to do
 				}
 			}
 		}
-		self::$cache->store("controllers/routes.default", "return " . UArray::asPhpArray($routes["default"], "array") . ";", 'controllers');
-		self::$cache->store("controllers/routes.rest", "return " . UArray::asPhpArray($routes["rest"], "array") . ";", 'controllers');
-		if (!$silent) {
+		self::$cache->store ( "controllers/routes.default", "return " . UArray::asPhpArray ( $routes ["default"], "array" ) . ";", 'controllers' );
+		self::$cache->store ( "controllers/routes.rest", "return " . UArray::asPhpArray ( $routes ["rest"], "array" ) . ";", 'controllers' );
+		if (! $silent) {
 			echo "Router cache reset\n";
 		}
 	}
 
 	private static function storeRouteResponse($key, $response) {
-		self::setKeyExpired($key, false);
-		self::$cache->store("controllers/" . $key, $response, 'controllers', false);
+		self::setKeyExpired ( $key, false );
+		self::$cache->store ( "controllers/" . $key, $response, 'controllers', false );
 		return $response;
 	}
 
 	private static function getRouteKey($routePath) {
-		return "path" . \md5(\implode("", $routePath));
+		return "path" . \md5 ( \implode ( "", $routePath ) );
 	}
 
-	private static function setKeyExpired($key, $expired=true) {
+	private static function setKeyExpired($key, $expired = true) {
 		if ($expired) {
-			self::$expiredRoutes[$key]=true;
+			self::$expiredRoutes [$key] = true;
 		} else {
-			unset(self::$expiredRoutes[$key]);
+			unset ( self::$expiredRoutes [$key] );
 		}
 	}
 
 	/**
+	 *
 	 * @param boolean $isRest
 	 * @return array
 	 */
-	public static function getControllerCache($isRest=false) {
-		$key=($isRest) ? "rest" : "default";
-		if (self::$cache->exists("controllers/routes." . $key))
-			return self::$cache->fetch("controllers/routes." . $key);
+	public static function getControllerCache($isRest = false) {
+		$key = ($isRest) ? "rest" : "default";
+		if (self::$cache->exists ( "controllers/routes." . $key ))
+			return self::$cache->fetch ( "controllers/routes." . $key );
 		return [ ];
 	}
 
 	public static function getRouteCache($routePath, $duration) {
-		$key=self::getRouteKey($routePath);
+		$key = self::getRouteKey ( $routePath );
 
-		if (self::$cache->exists("controllers/" . $key) && !self::expired($key, $duration)) {
-			$response=self::$cache->file_get_contents("controllers/" . $key);
+		if (self::$cache->exists ( "controllers/" . $key ) && ! self::expired ( $key, $duration )) {
+			$response = self::$cache->file_get_contents ( "controllers/" . $key );
 			return $response;
 		} else {
-			$response=Startup::runAsString($routePath);
-			return self::storeRouteResponse($key, $response);
+			$response = Startup::runAsString ( $routePath );
+			return self::storeRouteResponse ( $key, $response );
 		}
 	}
 
 	public static function expired($key, $duration) {
-		return self::$cache->expired("controllers/" . $key, $duration) === true || \array_key_exists($key, self::$expiredRoutes);
+		return self::$cache->expired ( "controllers/" . $key, $duration ) === true || \array_key_exists ( $key, self::$expiredRoutes );
 	}
 
 	public static function isExpired($path, $duration) {
-		$route=Router::getRoute($path, false);
-		if ($route !== false && \is_array($route)) {
-			return self::expired(self::getRouteKey($route), $duration);
+		$route = Router::getRoute ( $path, false );
+		if ($route !== false && \is_array ( $route )) {
+			return self::expired ( self::getRouteKey ( $route ), $duration );
 		}
 		return true;
 	}
 
-	public static function setExpired($routePath, $expired=true) {
-		$key=self::getRouteKey($routePath);
-		self::setKeyExpired($key, $expired);
+	public static function setExpired($routePath, $expired = true) {
+		$key = self::getRouteKey ( $routePath );
+		self::setKeyExpired ( $key, $expired );
 	}
 
 	public static function setRouteCache($routePath) {
-		$key=self::getRouteKey($routePath);
-		$response=Startup::runAsString($routePath);
-		return self::storeRouteResponse($key, $response);
+		$key = self::getRouteKey ( $routePath );
+		$response = Startup::runAsString ( $routePath );
+		return self::storeRouteResponse ( $key, $response );
 	}
 
 	public static function addAdminRoutes() {
-		self::addControllerCache("Ubiquity\controllers\Admin");
+		self::addControllerCache ( "Ubiquity\controllers\Admin" );
 	}
 
 	public static function getRoutes() {
-		$result=self::getControllerCache();
+		$result = self::getControllerCache ();
 		return $result;
 	}
 
-	public static function getControllerRoutes($controllerClass, $isRest=false) {
-		$result=[ ];
-		$ctrlCache=self::getControllerCache($isRest);
+	public static function getControllerRoutes($controllerClass, $isRest = false) {
+		$result = [ ];
+		$ctrlCache = self::getControllerCache ( $isRest );
 		foreach ( $ctrlCache as $path => $routeAttributes ) {
-			if (isset($routeAttributes["controller"])) {
-				if ($routeAttributes["controller"] === $controllerClass) {
-					$result[$path]=$routeAttributes;
+			if (isset ( $routeAttributes ["controller"] )) {
+				if ($routeAttributes ["controller"] === $controllerClass) {
+					$result [$path] = $routeAttributes;
 				}
 			} else {
-				$firstValue=current($routeAttributes);
-				if (isset($firstValue) && isset($firstValue["controller"])) {
-					if ($firstValue["controller"] === $controllerClass) {
-						$result[$path]=$routeAttributes;
+				$firstValue = current ( $routeAttributes );
+				if (isset ( $firstValue ) && isset ( $firstValue ["controller"] )) {
+					if ($firstValue ["controller"] === $controllerClass) {
+						$result [$path] = $routeAttributes;
 					}
 				}
 			}
@@ -149,56 +151,56 @@ trait RouterCacheTrait{
 		return $result;
 	}
 
-	public static function addRoute($path, $controller, $action="index", $methods=null, $name="") {
-		$controllerCache=self::getControllerCache();
-		Router::addRouteToRoutes($controllerCache, $path, $controller, $action, $methods, $name);
-		self::$cache->store("controllers/routes.default", "return " . UArray::asPhpArray($controllerCache, "array") . ";", 'controllers');
-	}
-	
-	public static function addRoutes($pathArray, $controller, $action="index", $methods=null, $name="") {
-		self::addRoutes_($pathArray, $controller,$action,$methods,$name,false);
-	}
-	
-	public static function addRestRoutes($pathArray, $controller, $action="index", $methods=null, $name="") {
-		self::addRoutes_($pathArray, $controller,$action,$methods,$name,true);
-	}
-	
-	private static function addRoutes_($pathArray, $controller, $action="index", $methods=null, $name="",$isRest=false) {
-		$controllerCache=self::getControllerCache($isRest);
-		$postfix="default";
-		if($isRest){
-			$postfix="rest";
-		}
-		Router::addRoutesToRoutes($controllerCache, $pathArray, $controller, $action, $methods, $name);
-		if(self::$minify){
-			ControllerParser::minifyRoutes($controllerCache);
-		}
-		self::$cache->store("controllers/routes.".$postfix, "return " . UArray::asPhpArray($controllerCache, "array") . ";", 'controllers');
+	public static function addRoute($path, $controller, $action = "index", $methods = null, $name = "") {
+		$controllerCache = self::getControllerCache ();
+		Router::addRouteToRoutes ( $controllerCache, $path, $controller, $action, $methods, $name );
+		self::$cache->store ( "controllers/routes.default", "return " . UArray::asPhpArray ( $controllerCache, "array" ) . ";", 'controllers' );
 	}
 
-	public static function getControllersFiles(&$config, $silent=false) {
-		return self::_getFiles($config, "controllers", $silent);
+	public static function addRoutes($pathArray, $controller, $action = "index", $methods = null, $name = "") {
+		self::addRoutes_ ( $pathArray, $controller, $action, $methods, $name, false );
 	}
 
-	public static function getControllers($subClass="\\Ubiquity\\controllers\\Controller",$backslash=false,$includeSubclass=false) {
-		$result=[ ];
-		if($includeSubclass){
-			$result[]=$subClass;
+	public static function addRestRoutes($pathArray, $controller, $action = "index", $methods = null, $name = "") {
+		self::addRoutes_ ( $pathArray, $controller, $action, $methods, $name, true );
+	}
+
+	private static function addRoutes_($pathArray, $controller, $action = "index", $methods = null, $name = "", $isRest = false) {
+		$controllerCache = self::getControllerCache ( $isRest );
+		$postfix = "default";
+		if ($isRest) {
+			$postfix = "rest";
 		}
-		$config=Startup::getConfig();
-		$files=self::getControllersFiles($config, true);
+		Router::addRoutesToRoutes ( $controllerCache, $pathArray, $controller, $action, $methods, $name );
+		if (self::$minify) {
+			ControllerParser::minifyRoutes ( $controllerCache );
+		}
+		self::$cache->store ( "controllers/routes." . $postfix, "return " . UArray::asPhpArray ( $controllerCache, "array" ) . ";", 'controllers' );
+	}
+
+	public static function getControllersFiles(&$config, $silent = false) {
+		return self::_getFiles ( $config, "controllers", $silent );
+	}
+
+	public static function getControllers($subClass = "\\Ubiquity\\controllers\\Controller", $backslash = false, $includeSubclass = false) {
+		$result = [ ];
+		if ($includeSubclass) {
+			$result [] = $subClass;
+		}
+		$config = Startup::getConfig ();
+		$files = self::getControllersFiles ( $config, true );
 		try {
-			$restCtrls=CacheManager::getRestCache();
+			$restCtrls = CacheManager::getRestCache ();
 		} catch ( \Exception $e ) {
-			$restCtrls=[ ];
+			$restCtrls = [ ];
 		}
 		foreach ( $files as $file ) {
-			if (is_file($file)) {
-				$controllerClass=ClassUtils::getClassFullNameFromFile($file,$backslash);
-				if (isset($restCtrls[$controllerClass]) === false) {
-					$r=new \ReflectionClass($controllerClass);
-					if($r->isSubclassOf($subClass) && !$r->isAbstract()){
-						$result[]=$controllerClass;
+			if (is_file ( $file )) {
+				$controllerClass = ClassUtils::getClassFullNameFromFile ( $file, $backslash );
+				if (class_exists ( $controllerClass ) && isset ( $restCtrls [$controllerClass] ) === false) {
+					$r = new \ReflectionClass ( $controllerClass );
+					if ($r->isSubclassOf ( $subClass ) && ! $r->isAbstract ()) {
+						$result [] = $controllerClass;
 					}
 				}
 			}
