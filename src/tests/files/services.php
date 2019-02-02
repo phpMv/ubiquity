21a7423060b19c457e688ed4364ec2abcc41b0b6
jcheron <myaddressmail@gmail.com>
2019-02-02 16:26:42 +0100

    Update services.php

--- src/tests/files/services.php
+++ src/tests/files/services.php
@@ -7,4 +7,4 @@ if ($db ["dbName"] !== "") {
 	\Ubiquity\orm\DAO::connect ( $db ["type"], $db ["dbName"], @$db ["serverName"], @$db ["port"], @$db ["user"], @$db ["password"], @$db ["options"], @$db ["cache"] );
 }
 Router::startAll ();
-Router::addRoute ( "_default", "controllers\Main" );
+Router::addRoute ( "_default", "controllers\\IndexController" );
