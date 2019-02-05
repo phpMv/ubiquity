8e405c9f56f0b18dad420eb0103832de734add91
jcheron <myaddressmail@gmail.com>
2019-02-05 18:36:24 +0100

    Update CrudOrgasCept.php

--- src/tests/functional/CrudOrgasCept.php
+++ src/tests/functional/CrudOrgasCept.php
@@ -5,12 +5,12 @@ $I->wantTo ( 'perform actions on crud controllers and see result' );
 $I->amOnPage ( "/TestCrudOrgas" );
 $I->see ( "lecnam.net" );
 // Test relation objects
-$I->click ( "tr[data-ajax='1']", "#dataTable" );
+$I->amOnPage ( "/TestCrudOrgas/showDetail/1" );
 $I->canSee ( "users (12)", "body" );
-// Test object insertion
-$I->click ( "._edit[data-ajax='1']", "#dataTable" );
+// Test object modification
+$I->amOnPage ( "/TestCrudOrgas/edit/no/1" );
 $I->canSee ( "Editing an existing object", "body" );
 $I->fillField ( "[name='aliases']", "cnam-basse-normandie.fr;cnam.fr" );
 $I->click ( "#action-modal-frmEdit-0" );
-$I->canSee ( "Modifications were successfully saved", "body" );
+$I->amOnPage ( "/TestCrudOrgas" );
 $I->canSee ( "cnam-basse-normandie.fr;cnam.fr", "tr[data-ajax='1'] td[data-field='aliases']" );
