e0029c55d7dfb209b9160c56bf9791b94bd45432
jcheron <myaddressmail@gmail.com>
2019-02-05 17:46:39 +0100

    sl corrections

--- src/tests/functional/CrudOrgasCept.php
+++ src/tests/functional/CrudOrgasCept.php
@@ -3,3 +3,5 @@ $I = new FunctionalTester ( $scenario );
 $I->wantTo ( 'perform actions on crud controllers and see result' );
 $I->amOnPage ( "/TestCrudOrgas" );
 $I->see ( "lecnam.net" );
+$I->click ( "tr[data-ajax='1']", "#dataTable" );
+$I->waitForText ( "users (12)", self::TIMEOUT, "body" );
