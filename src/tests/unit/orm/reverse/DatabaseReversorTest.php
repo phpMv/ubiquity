61e57b4438b084ebef375f9ddaea211fcb11abc6
jcheron <myaddressmail@gmail.com>
2019-02-06 18:34:58 +0100

    Create DatabaseReversorTest.php

---
+++ src/tests/unit/orm/reverse/DatabaseReversorTest.php
@@ -0,0 +1,34 @@
+<?php
+use Ubiquity\orm\reverse\DatabaseReversor;
+use Ubiquity\db\reverse\DbGenerator;
+use Ubiquity\controllers\Startup;
+use Ubiquity\utils\base\UString;
+
+/**
+ * DatabaseReversor test case.
+ */
+class DatabaseReversorTest extends BaseTest {
+	/**
+	 *
+	 * @var \Ubiquity\orm\reverse\DatabaseReversor
+	 */
+	private $databaseReversor;
+
+	/**
+	 * Tests DatabaseReversor->__construct()
+	 */
+	public function test__construct() {
+		Startup::setConfig ( $this->config );
+		$this->databaseReversor = new DatabaseReversor ( new DbGenerator () );
+	}
+
+	/**
+	 * Tests DatabaseReversor->createDatabase()
+	 */
+	public function testCreateDatabase() {
+		$this->databaseReversor->createDatabase ( "testDb" );
+		$script = $this->databaseReversor->__toString ();
+		$this->assertTrue ( UString::contains ( "CREATE DATABASE", $script ) );
+	}
+}
+
