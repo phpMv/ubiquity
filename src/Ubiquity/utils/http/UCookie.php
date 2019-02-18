687c6883008ef5235088703ca6dd177c2382f6ea
jcheron <myaddressmail@gmail.com>
2019-02-18 15:57:38 +0100

    Fix UCookie test bug

--- src/Ubiquity/utils/http/UCookie.php
+++ src/Ubiquity/utils/http/UCookie.php
@@ -4,50 +4,62 @@ namespace Ubiquity\utils\http;
 
 /**
  * Http Cookies utilities
- * @author jc
- * @version 1.0.0.2
+ * Ubiquity\utils\http$UCookie
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.2
+ *
  */
 class UCookie {
 
 	/**
 	 * Sends a cookie
-	 * @param string $name the name of the cookie
-	 * @param string $value The value of the cookie.
-	 * @param int $duration default : 1 day
-	 * @param string $path default : / the cookie will be available within the entire domain
+	 *
+	 * @param string $name
+	 *        	the name of the cookie
+	 * @param string $value
+	 *        	The value of the cookie.
+	 * @param int $duration
+	 *        	default : 1 day
+	 * @param string $path
+	 *        	default : / the cookie will be available within the entire domain
 	 */
-	public static function set($name, $value, $duration=60*60*24, $path="/") {
-		\setcookie($name, $value, \time() + $duration, $path);
+	public static function set($name, $value, $duration = 60*60*24, $path = "/") {
+		\setcookie ( $name, $value, \time () + $duration, $path );
 	}
 
 	/**
 	 * Returns the Cookie with the name $name
+	 *
 	 * @param string $name
 	 * @param string $default
 	 * @return null|string
 	 */
-	public static function get($name, $default=null) {
-		return isset($_COOKIE[$name]) ? $_COOKIE[$name] : $default;
+	public static function get($name, $default = null) {
+		return isset ( $_COOKIE [$name] ) ? $_COOKIE [$name] : $default;
 	}
 
 	/**
 	 * Removes the cookie with the name $name
+	 *
 	 * @param string $name
-	 * @param $path
+	 * @param
+	 *        	$path
 	 */
-	public static function delete($name, $path="/") {
-		if(isset($_COOKIE[$name])){
-			unset($_COOKIE[$name]);
+	public static function delete($name, $path = "/") {
+		if (isset ( $_COOKIE [$name] )) {
+			unset ( $_COOKIE [$name] );
 		}
-		\setcookie($name, "", \time() - 3600, $path);
+		\setcookie ( $name, "", \time () - 3600, $path );
 	}
 
 	/**
 	 * Deletes all cookies
 	 */
-	public function deleteAll($path="/") {
+	public static function deleteAll($path = "/") {
 		foreach ( $_COOKIE as $name => $value ) {
-			self::delete($name, $path);
+			self::delete ( $name, $path );
 		}
 	}
 }
--- src/tests/files/unit/controllers/TestUCookieController.php
+++ src/tests/files/unit/controllers/TestUCookieController.php
@@ -8,17 +8,34 @@ class TestUCookieController extends ControllerBase {
 
 	public function index() {
 		UCookie::set ( "user", "test-user" );
+		UCookie::set ( "other", "pwd" );
 		echo "cookie";
 	}
 
 	public function testUser() {
-		if (UCookie::exists ( "user" ))
-			echo UCookie::get ( "user" );
+		echo UCookie::get ( "user", "not-exists" );
+	}
+
+	public function testOther() {
+		echo UCookie::get ( "other", "not-exists" );
+	}
+
+	public function testNotExists() {
+		echo UCookie::get ( "not-exists", "not-exists" );
 	}
 
 	public function delete() {
 		UCookie::delete ( "user" );
 		echo "deleted";
+	}
+
+	public function deleteAll() {
+		UCookie::deleteAll ();
+		echo "deleteds";
+	}
+
+	public function testAllDeleted() {
+		echo UCookie::get ( "user", "no" ) . ':' . UCookie::get ( "other", "no" );
 	}
 
 	public function testDeleted() {
