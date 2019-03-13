bbba5cf59257edbf448c2599af108e2a8623bc01
jcheron <myaddressmail@gmail.com>
2019-03-13 16:34:38 +0100

    [Fix] Twig 2.7 depreciations

--- src/Ubiquity/views/engine/TemplateEngine.php
+++ src/Ubiquity/views/engine/TemplateEngine.php
@@ -2,9 +2,37 @@
 
 namespace Ubiquity\views\engine;
 
+/**
+ * Abstract template engine.
+ * Ubiquity\views\engine$TemplateEngine
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.0
+ *
+ */
 abstract class TemplateEngine {
 
+	/**
+	 * Renders a view.
+	 *
+	 * @param string $fileName
+	 * @param array|null $pData
+	 * @param boolean $asString
+	 */
 	abstract public function render($fileName, $pData, $asString);
+
+	/**
+	 * Returns the defined block names.
+	 *
+	 * @param string $templateName
+	 */
 	abstract public function getBlockNames($templateName);
+
+	/**
+	 * Returns the source code of the template
+	 *
+	 * @param string $templateName
+	 */
 	abstract public function getCode($templateName);
 }
--- src/Ubiquity/views/engine/Twig.php
+++ src/Ubiquity/views/engine/Twig.php
@@ -8,47 +8,53 @@ use Ubiquity\cache\CacheManager;
 use Ubiquity\core\Framework;
 use Ubiquity\utils\base\UFileSystem;
 use Ubiquity\translation\TranslatorManager;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\TwigFunction;
+use Twig\TwigTest;
 
 /**
- * Ubiquity Twig template engine
+ * Ubiquity Twig template engine.
  *
  * Ubiquity\views\engine$Twig
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.3
+ * @version 1.0.5
  *
  */
 class Twig extends TemplateEngine {
 	private $twig;
+	private $loader;
 
 	public function __construct($options = array()) {
-		$loader = new \Twig_Loader_Filesystem ( \ROOT . \DS . "views" . \DS );
+		$loader = new FilesystemLoader ( \ROOT . \DS . "views" . \DS );
 		$loader->addPath ( implode ( \DS, [ Startup::getFrameworkDir (),"..","core","views" ] ) . \DS, "framework" );
 		if (isset ( $options ["cache"] ) && $options ["cache"] === true)
 			$options ["cache"] = CacheManager::getCacheSubDirectory ( "views" );
-		$this->twig = new \Twig_Environment ( $loader, $options );
+		$this->twig = new Environment ( $loader, $options );
 
-		$function = new \Twig_SimpleFunction ( 'path', function ($name, $params = [], $absolute = false) {
+		$function = new TwigFunction ( 'path', function ($name, $params = [], $absolute = false) {
 			return Router::path ( $name, $params, $absolute );
 		} );
 		$this->twig->addFunction ( $function );
-		$function = new \Twig_SimpleFunction ( 'url', function ($name, $params) {
+		$function = new TwigFunction ( 'url', function ($name, $params) {
 			return Router::url ( $name, $params );
 		} );
 		$this->twig->addFunction ( $function );
 
-		$function = new \Twig_SimpleFunction ( 't', function ($context, $id, array $parameters = array(), $domain = null, $locale = null) {
+		$function = new TwigFunction ( 't', function ($context, $id, array $parameters = array(), $domain = null, $locale = null) {
 			$trans = TranslatorManager::trans ( $id, $parameters, $domain, $locale );
 			return $this->twig->createTemplate ( $trans )->render ( $context );
 		}, [ 'needs_context' => true ] );
 		$this->twig->addFunction ( $function );
 
-		$test = new \Twig_SimpleTest ( 'instanceOf', function ($var, $class) {
+		$test = new TwigTest ( 'instanceOf', function ($var, $class) {
 			return $var instanceof $class;
 		} );
 		$this->twig->addTest ( $test );
 		$this->twig->addGlobal ( "app", new Framework () );
+		$this->loader = $loader;
 	}
 
 	/*
@@ -65,11 +71,33 @@ class Twig extends TemplateEngine {
 		}
 	}
 
+	/**
+	 *
+	 * {@inheritdoc}
+	 * @see \Ubiquity\views\engine\TemplateEngine::getBlockNames()
+	 */
 	public function getBlockNames($templateName) {
 		return $this->twig->load ( $templateName )->getBlockNames ();
 	}
 
+	/**
+	 *
+	 * {@inheritdoc}
+	 * @see \Ubiquity\views\engine\TemplateEngine::getCode()
+	 */
 	public function getCode($templateName) {
 		return UFileSystem::load ( $this->twig->load ( $templateName )->getSourceContext ()->getPath () );
+	}
+
+	/**
+	 * Adds a new path in a namespace
+	 *
+	 * @param string $path
+	 *        	The path to add
+	 * @param string $namespace
+	 *        	The namespace to use
+	 */
+	public function addPath(string $path, string $namespace) {
+		$this->loader->addPath ( $path, $namespace );
 	}
 }
