de31bc4bf410fc840d1da181c703cd3ca62c9cc4
jcheron <myaddressmail@gmail.com>
2019-03-14 15:32:46 +0100

    [add] view events (before and after rendering)

--- src/Ubiquity/themes/ThemesManager.php
+++ src/Ubiquity/themes/ThemesManager.php
@@ -1,137 +1,97 @@
 <?php
 
-namespace Ubiquity\assets;
+namespace Ubiquity\themes;
 
-use Ubiquity\themes\ThemesManager;
+use Ubiquity\controllers\Startup;
+use Ubiquity\views\engine\Twig;
+use Ubiquity\exceptions\ThemesException;
+use Ubiquity\events\EventsManager;
+use Ubiquity\events\ViewEvents;
 use Ubiquity\utils\base\UArray;
 
 /**
- * Assets manager for css and js inclusions in templates.
- * Ubiquity\assets$AssetsManager
+ * Themes manager.
+ * Ubiquity\themes$ThemesManager
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.1
+ * @version 1.0.0
  * @since Ubiquity 2.1.0
  *
  */
-class AssetsManager {
-	const ASSETS_FOLDER = '/public/assets/';
-	private static $siteURL;
+class ThemesManager {
+	const THEMES_FOLDER = 'themes';
+	private static $activeTheme;
 
-	private static function gString($template, $variable, $attributes = []) {
-		$implode = UArray::implodeAsso ( $attributes, ' ' );
-		return sprintf ( $template, $variable, $implode );
-	}
-
-	private static function script($src, $attributes = []) {
-		return self::gString ( '<script type="text/javascript" src="%s" %s></script>', $src, $attributes );
-	}
-
-	private static function stylesheet($link, $attributes = []) {
-		return self::gString ( '<link href="%s" type="text/css" rel="stylesheet" %s>', $link, $attributes );
+	public static function getActiveTheme() {
+		return self::$activeTheme;
 	}
 
 	/**
-	 * Starts the assets manager.
-	 * Essential to define the siteURL part
+	 * Sets the activeTheme
 	 *
-	 * @param array $config
+	 * @param string $activeTheme
+	 * @throws ThemesException
 	 */
-	public static function start(&$config) {
-		$siteURL = $config ['siteUrl'] ?? '';
-		self::$siteURL = rtrim ( $siteURL, '/' );
-	}
-
-	/**
-	 * Returns the absolute or relative url to the resource.
-	 *
-	 * @param string $resource
-	 * @param boolean $absolute
-	 * @return string
-	 */
-	public static function getUrl($resource, $absolute = false) {
-		if (strpos ( $resource, '//' ) !== false) {
-			return $resource;
+	public static function setActiveTheme($activeTheme) {
+		self::$activeTheme = $activeTheme ?? '';
+		$engineInstance = Startup::$templateEngine;
+		if ($engineInstance instanceof Twig) {
+			$engineInstance->setTheme ( $activeTheme, self::THEMES_FOLDER );
+		} else {
+			throw new ThemesException ( 'Template engine must be an instance of Twig for themes activation!' );
 		}
-		if ($absolute) {
-			return self::$siteURL . self::ASSETS_FOLDER . $resource;
+	}
+
+	public static function saveActiveTheme($theme) {
+		$config = Startup::getConfig ();
+		$config ['templateEngineOptions'] ['activeTheme'] = $theme;
+		$content = "<?php\nreturn " . UArray::asPhpArray ( $config, "array", 1, true ) . ";";
+		Startup::saveConfig ( $content );
+		return $config;
+	}
+
+	/**
+	 * Sets the activeTheme
+	 *
+	 * @param string $activeTheme
+	 */
+	public static function setActiveThemeFromTwig($activeTheme) {
+		self::$activeTheme = $activeTheme;
+	}
+
+	/**
+	 * Returns the names of available themes.
+	 *
+	 * @return string[]
+	 */
+	public static function getAvailableThemes() {
+		$path = \ROOT . \DS . 'views' . \DS . self::THEMES_FOLDER . \DS . '*';
+		$dirs = \glob ( $path, GLOB_ONLYDIR | GLOB_NOSORT );
+		$result = [ ];
+		foreach ( $dirs as $dir ) {
+			$result [] = basename ( $dir );
 		}
-		return ltrim ( self::ASSETS_FOLDER, '/' ) . $resource;
+		return $result;
 	}
 
 	/**
-	 * Returns the absolute or relative url for a resource in the **activeTheme**.
+	 * Adds a listener before theme rendering.
+	 * The callback function takes the following parameters: $view (the view name), $pData (array of datas sent to the view)
 	 *
-	 * @param string $resource
-	 * @param boolean $absolute
-	 * @return string
+	 * @param callable $callback
 	 */
-	public static function getActiveThemeUrl($resource, $absolute = false) {
-		$activeTheme = ThemesManager::getActiveTheme ();
-		return self::getThemeUrl ( $activeTheme, $resource, $absolute );
+	public static function onBeforeRender($callback) {
+		EventsManager::addListener ( ViewEvents::BEFORE_RENDER, $callback );
 	}
 
 	/**
-	 * Returns the absolute or relative url for a resource in a theme.
+	 * Adds a listener after theme rendering.
+	 * The callback function takes the following parameters: $render (the response string), $view (the view name), $pData (array of datas sent to the view)
 	 *
-	 * @param string $theme
-	 * @param string $resource
-	 * @param boolean $absolute
-	 * @return string
+	 * @param callable $callback
 	 */
-	public static function getThemeUrl($theme, $resource, $absolute = false) {
-		if ($absolute) {
-			return self::$siteURL . self::ASSETS_FOLDER . $theme . '/' . $resource;
-		}
-		return ltrim ( self::ASSETS_FOLDER, '/' ) . $theme . '/' . $resource;
-	}
-
-	/**
-	 * Returns the script inclusion for a javascript resource.
-	 *
-	 * @param string $resource The javascript resource to include
-	 * @param array $attributes The other html attributes of the script element
-	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
-	 * @return string
-	 */
-	public static function js($resource, $attributes = [], $absolute = false) {
-		return self::script ( self::getUrl ( $resource, $absolute ), $attributes );
-	}
-
-	/**
-	 * Returns the css inclusion for a stylesheet resource.
-	 *
-	 * @param string $resource The css resource to include
-	 * @param array $attributes The other html attributes of the script element
-	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
-	 * @return string
-	 */
-	public static function css($resource, $attributes = [], $absolute = false) {
-		return self::stylesheet ( self::getUrl ( $resource, $absolute ), $attributes );
-	}
-
-	/**
-	 * Returns the script inclusion for a javascript resource in **activeTheme**.
-	 *
-	 * @param string $resource The javascript resource to include
-	 * @param array $attributes The other html attributes of the script element
-	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
-	 * @return string
-	 */
-	public static function js_($resource, $attributes = [], $absolute = false) {
-		return self::script ( self::getActiveThemeUrl ( $resource, $absolute ), $attributes );
-	}
-
-	/**
-	 * Returns the css inclusion for a stylesheet resource in **activeTheme**.
-	 *
-	 * @param string $resource The css resource to include
-	 * @param array $attributes The other html attributes of the script element
-	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
-	 * @return string
-	 */
-	public static function css_($resource, $attributes = [], $absolute = false) {
-		return self::stylesheet ( self::getActiveThemeUrl ( $resource, $absolute ), $attributes );
+	public static function onAfterRender($callback) {
+		EventsManager::addListener ( ViewEvents::AFTER_RENDER, $callback );
 	}
 }
