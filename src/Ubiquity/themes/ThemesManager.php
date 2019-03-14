6b522528a0db17471e931646d884e3552e1f3b37
jcheron <myaddressmail@gmail.com>
2019-03-14 15:32:46 +0100

    [add] view events (before and after rendering)

--- src/Ubiquity/themes/ThemesManager.php
+++ src/Ubiquity/themes/ThemesManager.php
@@ -5,6 +5,9 @@ namespace Ubiquity\themes;
 use Ubiquity\controllers\Startup;
 use Ubiquity\views\engine\Twig;
 use Ubiquity\exceptions\ThemesException;
+use Ubiquity\events\EventsManager;
+use Ubiquity\events\ViewEvents;
+use Ubiquity\utils\base\UFileSystem;
 
 /**
  * Themes manager.
@@ -28,6 +31,12 @@ class ThemesManager {
 		return self::$activeTheme;
 	}
 
+	/**
+	 * Sets the activeTheme
+	 *
+	 * @param string $activeTheme
+	 * @throws ThemesException
+	 */
 	public static function setActiveTheme($activeTheme) {
 		self::$activeTheme = $activeTheme ?? 'default';
 		$engineInstance = Startup::getTempateEngineInstance ();
@@ -36,6 +45,41 @@ class ThemesManager {
 		} else {
 			throw new ThemesException ( 'Template engine must be an instance of Twig for themes activation!' );
 		}
+	}
+
+	/**
+	 * Returns the names of available themes.
+	 *
+	 * @return string[]
+	 */
+	public static function getAvailableThemes() {
+		$path = \ROOT . \DS . 'views' . \DS . self::THEMES_FOLDER . \DS . '*';
+		$dirs = \glob ( $path, GLOB_ONLYDIR | GLOB_NOSORT );
+		$result = [ ];
+		foreach ( $dirs as $dir ) {
+			$result [] = basename ( $dir );
+		}
+		return $result;
+	}
+
+	/**
+	 * Adds a listener before theme rendering.
+	 * The callback function takes the following parameters: $view (the view name), $pData (array od datas sent to the view)
+	 *
+	 * @param callable $callback
+	 */
+	public static function onBeforeRender($callback) {
+		EventsManager::addListener ( ViewEvents::BEFORE_RENDER, $callback );
+	}
+
+	/**
+	 * Adds a listener after theme rendering.
+	 * The callback function takes the following parameters: $render (the response string), $view (the view name), $pData (array od datas sent to the view)
+	 *
+	 * @param callable $callback
+	 */
+	public static function onAfterRender($callback) {
+		EventsManager::addListener ( ViewEvents::AFTER_RENDER, $callback );
 	}
 }
 
