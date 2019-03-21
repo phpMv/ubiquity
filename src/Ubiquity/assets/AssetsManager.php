e6b17f6967b1b3b4822fd3aba80585537ec7c1ff
jcheron <myaddressmail@gmail.com>
2019-03-21 01:58:22 +0100

    [add] extra attributes for css and js functions (crossorigin,integrity...)

--- src/Ubiquity/assets/AssetsManager.php
+++ src/Ubiquity/assets/AssetsManager.php
@@ -3,6 +3,7 @@
 namespace Ubiquity\assets;
 
 use Ubiquity\themes\ThemesManager;
+use Ubiquity\utils\base\UArray;
 
 /**
  * Assets manager for css and js inclusions in templates.
@@ -10,7 +11,7 @@ use Ubiquity\themes\ThemesManager;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.0
+ * @version 1.0.1
  * @since Ubiquity 2.1.0
  *
  */
@@ -18,12 +19,17 @@ class AssetsManager {
 	const ASSETS_FOLDER = '/public/assets/';
 	private static $siteURL;
 
-	private static function script($src) {
-		return sprintf ( '<script type="text/javascript" src="%s"></script>', $src );
+	private static function gString($template, $variable, $attributes = []) {
+		$implode = UArray::implodeAsso ( $attributes, ' ' );
+		return sprintf ( $template, $variable, $implode );
 	}
 
-	private static function stylesheet($link) {
-		return sprintf ( '<link href="%s" type="text/css" rel="stylesheet">', $link );
+	private static function script($src, $attributes = []) {
+		return self::gString ( '<script type="text/javascript" src="%s" %s></script>', $src, $attributes );
+	}
+
+	private static function stylesheet($link, $attributes = []) {
+		return self::gString ( '<link href="%s" type="text/css" rel="stylesheet" %s>', $link, $attributes );
 	}
 
 	/**
@@ -51,7 +57,7 @@ class AssetsManager {
 		if ($absolute) {
 			return self::$siteURL . self::ASSETS_FOLDER . $resource;
 		}
-		return ltrim(self::ASSETS_FOLDER,'/') . $resource;
+		return ltrim ( self::ASSETS_FOLDER, '/' ) . $resource;
 	}
 
 	/**
@@ -78,51 +84,55 @@ class AssetsManager {
 		if ($absolute) {
 			return self::$siteURL . self::ASSETS_FOLDER . $theme . '/' . $resource;
 		}
-		return ltrim(self::ASSETS_FOLDER,'/') . $theme . '/' . $resource;
+		return ltrim ( self::ASSETS_FOLDER, '/' ) . $theme . '/' . $resource;
 	}
 
 	/**
-	 * Returns the script inclusion for a javascript resource
+	 * Returns the script inclusion for a javascript resource.
 	 *
 	 * @param string $resource The javascript resource to include
+	 * @param array $attributes The other html attributes of the script element
 	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
 	 * @return string
 	 */
-	public static function js($resource, $absolute = false) {
-		return self::script ( self::getUrl ( $resource, $absolute ) );
+	public static function js($resource, $attributes = [], $absolute = false) {
+		return self::script ( self::getUrl ( $resource, $absolute ), $attributes );
 	}
 
 	/**
-	 * Returns the css inclusion for a stylesheet resource
+	 * Returns the css inclusion for a stylesheet resource.
 	 *
 	 * @param string $resource The css resource to include
+	 * @param array $attributes The other html attributes of the script element
 	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
 	 * @return string
 	 */
-	public static function css($resource, $absolute = false) {
-		return self::stylesheet ( self::getUrl ( $resource, $absolute ) );
+	public static function css($resource, $attributes = [], $absolute = false) {
+		return self::stylesheet ( self::getUrl ( $resource, $absolute ), $attributes );
 	}
 
 	/**
-	 * Returns the script inclusion for a javascript resource in **activeTheme**
+	 * Returns the script inclusion for a javascript resource in **activeTheme**.
 	 *
 	 * @param string $resource The javascript resource to include
+	 * @param array $attributes The other html attributes of the script element
 	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
 	 * @return string
 	 */
-	public static function js_($resource, $absolute = false) {
-		return self::script ( self::getActiveThemeUrl ( $resource, $absolute ) );
+	public static function js_($resource, $attributes = [], $absolute = false) {
+		return self::script ( self::getActiveThemeUrl ( $resource, $absolute ), $attributes );
 	}
 
 	/**
-	 * Returns the css inclusion for a stylesheet resource in **activeTheme**
+	 * Returns the css inclusion for a stylesheet resource in **activeTheme**.
 	 *
 	 * @param string $resource The css resource to include
+	 * @param array $attributes The other html attributes of the script element
 	 * @param boolean $absolute True if url must be absolute (containing siteUrl)
 	 * @return string
 	 */
-	public static function css_($resource, $absolute = false) {
-		return self::stylesheet ( self::getActiveThemeUrl ( $resource, $absolute ) );
+	public static function css_($resource, $attributes = [], $absolute = false) {
+		return self::stylesheet ( self::getActiveThemeUrl ( $resource, $absolute ), $attributes );
 	}
 }
 
