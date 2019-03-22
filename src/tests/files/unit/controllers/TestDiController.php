d43aead21cf4dca5d0877af726308ab947170916
jcheron <myaddressmail@gmail.com>
2019-03-22 02:41:20 +0100

    [Add] di acceptance tests

--- src/tests/files/unit/controllers/TestDiController.php
+++ src/tests/files/unit/controllers/TestDiController.php
@@ -1,18 +1,51 @@
 <?php
+
 namespace controllers;
- use services\IService;
- use Ubiquity\controllers\Controller;
- 
- /**
+
+use services\IService;
+use Ubiquity\controllers\Controller;
+use services\IInjected;
+use Ubiquity\cache\CacheManager;
+use Ubiquity\controllers\Startup;
+
+/**
  * Controller TestDiController
- **/
-class TestDiController extends Controller{
-    /**
-     * @autowired
-     * @var IService
-     */
-    private $iService;
-	public function index(){
-		
+ *
+ * @property \services\IAllService allS
+ */
+class TestDiController extends Controller {
+	/**
+	 *
+	 * @autowired
+	 * @var IService
+	 */
+	private $iService;
+
+	public function initCache() {
+		$config = Startup::getConfig ();
+		CacheManager::start ( $config );
+		CacheManager::initCache ( $config );
+	}
+
+	/**
+	 *
+	 * @injected
+	 * @var IInjected
+	 */
+	private $inj;
+
+	public function index() {
+	}
+
+	public function autowired() {
+		$this->iService->do ( "!:autowired:!" );
+	}
+
+	public function injected() {
+		$this->inj->do ( "!:injected:!" );
+	}
+
+	public function allInjected() {
+		$this->allS->do ( "!:*injected:!" );
 	}
 }
