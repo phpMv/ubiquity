100fcf485abd69a6201b6199862b5190c250de27
jcheron <myaddressmail@gmail.com>
2019-04-11 02:41:54 +0200

    [test] transform methods

--- src/tests/unit/contents/TransformersManagerTest.php
+++ src/tests/unit/contents/TransformersManagerTest.php
@@ -1,5 +1,6 @@
 <?php
 use models\User;
+use models\Connection;
 use Ubiquity\contents\transformation\TransformersManager;
 use Ubiquity\orm\DAO;
 use Ubiquity\controllers\Startup;
@@ -24,6 +25,7 @@ class TransformersManagerTest extends BaseTest {
 			DAO::connect ( $db ["type"] ?? 'mysql', $db ["dbName"], $db ["serverName"] ?? '127.0.0.1', $db ["port"] ?? 3306, $db ["user"] ?? 'root', $db ["password"] ?? '', $db ["options"] ?? [ ], $db ["cache"] ?? false);
 		}
 		TransformersManager::startProd ();
+		DAO::$transformerOp = 'transform';
 	}
 
 	/**
@@ -50,12 +52,38 @@ class TransformersManagerTest extends BaseTest {
 		$metaDatas = OrmUtils::getModelMetadata ( User::class );
 		$transformers = $metaDatas ["#transformers"] ['toView'] ?? [ ];
 		$this->assertEquals ( 1, sizeof ( $transformers ) );
-		DAO::$transformerOp = 'transform';
 		$user = DAO::getOne ( User::class, 'id=1' );
 		$password = $user->getPassword ();
 		DAO::$transformerOp = 'toView';
 		$user2 = DAO::getOne ( User::class, 'id=1' );
 		$this->assertEquals ( sha1 ( $password ), $user2->getPassword () );
+	}
+
+	/**
+	 * Tests DateTime transformer
+	 */
+	public function testDateTime() {
+		$co = DAO::getOne ( Connection::class, 1 );
+		$dt = $co->getDateCo ();
+		$this->assertInstanceOf ( DateTime::class, get_class ( $dt ) );
+		DAO::$transformerOp = 'toView';
+		$co = DAO::getOne ( Connection::class, 1 );
+		$this->assertEquals ( $dt->format ( 'Y-m-d H:i:s' ), $co->getDateCo () );
+		DAO::$transformerOp = 'toForm';
+		$co = DAO::getOne ( Connection::class, 1 );
+		$this->assertEquals ( $dt->format ( 'Y-m-d\TH:i:s' ), $co->getDateCo () );
+	}
+
+	public function testTransformFunctions() {
+		DAO::$useTransformers = false;
+		$co = DAO::getOne ( Connection::class, 1 );
+		TransformersManager::transformInstance ( $co );
+		$dt = $co->getDateCo ();
+		$this->assertInstanceOf ( DateTime::class, get_class ( $dt ) );
+		$co = DAO::getOne ( Connection::class, 1 );
+		TransformersManager::transform ( $co, 'dateCo' );
+		$dt = $co->getDateCo ();
+		$this->assertInstanceOf ( DateTime::class, get_class ( $dt ) );
 	}
 
 	protected function getCacheDirectory() {
