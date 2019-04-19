a40d552f97d01546e236ce4b2cc339dd4621d027
jcheron <myaddressmail@gmail.com>
2019-04-19 03:56:46 +0200

    Update TestRestSimpleOrga.php

--- src/tests/files/unit/controllers/TestRestSimpleOrga.php
+++ src/tests/files/unit/controllers/TestRestSimpleOrga.php
@@ -7,5 +7,8 @@ namespace controllers;
  * @rest("resource"=>"models\\Organization")
  */
 class TestRestSimpleOrga extends \Ubiquity\controllers\rest\SimpleRestController {
-
+	
+	public function isValid($action){
+		return true;
+	}
 }
