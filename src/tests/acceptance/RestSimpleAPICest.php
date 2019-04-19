8768a0bf1ff3b0925cf48ad55610f5e0dde8a2d4
jcheron <myaddressmail@gmail.com>
2019-04-19 02:05:33 +0200

    Update RestSimpleAPICest.php

--- src/tests/acceptance/RestSimpleAPICest.php
+++ src/tests/acceptance/RestSimpleAPICest.php
@@ -86,16 +86,16 @@ class RestSimpleAPICest extends BaseAcceptance {
 		$I->fillField ( '#url', '/rest/simple/orgas/' );
 		$I->fillField ( '#method', 'post' );
 		$I->fillField ( '#datas', "{name:'microsoft',domain:'microsoft.com'}" );
-		$I->click ( "#btSubmit" );
+		$this->waitAndclick ( $I, "#btSubmit" );
 		$I->waitForText ( 'inserted', self::TIMEOUT );
 		$id = $I->grabTextFrom ( "#newId" );
 		$I->fillField ( '#url', '/rest/simple/orgas/' . $id );
 		$I->fillField ( '#method', 'patch' );
 		$I->fillField ( '#datas', "{name:'microsoft2',domain:'microsoft2.com'}" );
-		$I->click ( "#btSubmit" );
+		$this->waitAndclick ( $I, "#btSubmit" );
 		$I->waitForText ( 'updated', self::TIMEOUT );
 		$I->fillField ( '#method', 'delete' );
-		$I->click ( "#btSubmit" );
+		$this->waitAndclick ( $I, "#btSubmit" );
 		$I->waitForText ( 'deleted', self::TIMEOUT );
 	}
 }
