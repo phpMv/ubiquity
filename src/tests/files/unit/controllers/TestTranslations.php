2d65079e74fe87d89b3d6683171881c765da2ee0
jcheron <myaddressmail@gmail.com>
2019-06-12 01:32:10 +0200

    Update TestTranslations.php

--- src/tests/files/unit/controllers/TestTranslations.php
+++ src/tests/files/unit/controllers/TestTranslations.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace controllers;
 
 use Ubiquity\translation\TranslatorManager;
+use Ubiquity\utils\http\URequest;
 
 /**
  * Controller TestTranslations
@@ -9,16 +11,16 @@ use Ubiquity\translation\TranslatorManager;
 class TestTranslations extends ControllerBase {
 
 	public function index() {
-		TranslatorManager::start();
-		$this->_index();
+		TranslatorManager::start ();
+		$this->_index ( TranslatorManager::fixLocale ( URequest::getDefaultLanguage () ) );
 	}
 
 	private function _index($nb) {
-		$this->loadView("TestTranslations/index.html", compact('nb'));
+		$this->loadView ( "TestTranslations/index.html", compact ( 'nb' ) );
 	}
 
 	public function changeLocale($locale = 'en_EN', $nb = 0) {
-		TranslatorManager::start($locale);
-		$this->_index($nb);
+		TranslatorManager::start ( $locale );
+		$this->_index ( $nb );
 	}
 }
