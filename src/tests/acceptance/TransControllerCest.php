c3fc2ca91478aafa3aa778043972a3bdc38409c4
jcheron <myaddressmail@gmail.com>
2019-06-12 02:13:44 +0200

    Update TransControllerCest.php

--- src/tests/acceptance/TransControllerCest.php
+++ src/tests/acceptance/TransControllerCest.php
@@ -11,31 +11,31 @@ class TransControllerCest {
 		$I->see ( "Translation" );
 		$I->see ( "no translation" );
 
-		$I->amOnPage ( "/TestTranslations/fr_FR" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/fr_FR" );
 		$I->see ( "Traduction" );
 		$I->see ( "Aucune traduction" );
 
-		$I->amOnPage ( "/TestTranslations/en_EN" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/en_EN" );
 		$I->see ( "Translation" );
 		$I->see ( "no translation" );
 
-		$I->amOnPage ( "/TestTranslations/fr_FR/1" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/fr_FR/1" );
 		$I->see ( "Traduction" );
 		$I->see ( "Une traduction" );
 
-		$I->amOnPage ( "/TestTranslations/en_EN/1" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/en_EN/1" );
 		$I->see ( "Translation" );
 		$I->see ( "One translation" );
 
-		$I->amOnPage ( "/TestTranslations/fr_FR/5" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/fr_FR/5" );
 		$I->see ( "Traduction" );
 		$I->see ( "5 traductions" );
 
-		$I->amOnPage ( "/TestTranslations/en_EN/10" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/en_EN/10" );
 		$I->see ( "Translation" );
 		$I->see ( "10 translations" );
 
-		$I->amOnPage ( "/TestTranslations/blop/5" );
+		$I->amOnPage ( "/TestTranslations/changeLocale/blop/5" );
 		$I->see ( "trans" );
 		$I->see ( "multi.trans" );
 	}
