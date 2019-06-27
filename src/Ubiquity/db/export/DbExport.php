ec356331ae13b2ada4dbbc8de48f8a8f2792ec8e
jcheron <myaddressmail@gmail.com>
2019-06-27 02:51:13 +0200

    Update DbExport.php

--- src/Ubiquity/db/export/DbExport.php
+++ src/Ubiquity/db/export/DbExport.php
@@ -1,37 +1,48 @@
 <?php
+
 namespace Ubiquity\db\export;
 
 use Ubiquity\controllers\Startup;
 use Ubiquity\cache\CacheManager;
 use Ubiquity\orm\parser\ManyToManyParser;
+use Ubiquity\db\SqlUtils;
 
-class DbExport{
-	protected $manyToManys=[];
+/**
+ * Exports existing models to SQL
+ * Ubiquity\db\export$DbExport
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.1
+ *
+ */
+class DbExport {
+	protected $manyToManys = [ ];
 
-	public function __construct(){
-
+	public function __construct() {
 	}
 
-	public function exports(){
-		$result=[];
-		$config=Startup::getConfig();
-		$models=CacheManager::getModels($config,true);
-		foreach ($models as $model){
-			$tableExport=new TableExport($model);
-			$result[]=$tableExport->exports($this);
+	public function exports() {
+		$result = [ ];
+		$config = Startup::getConfig ();
+		$models = CacheManager::getModels ( $config, true );
+		foreach ( $models as $model ) {
+			$tableExport = new TableExport ( $model );
+			$result [] = $tableExport->exports ( $this );
 		}
-		foreach ($this->manyToManys as $target=>$ManyToManyParser){
-			$ManyToManyParser->init();
-			$sqlExport=new SqlExport();
-			$fields=[$ManyToManyParser->getFkField(),$ManyToManyParser->getMyFkField()];
-			$result[]=$sqlExport->exports($target,$fields);
+		foreach ( $this->manyToManys as $target => $ManyToManyParser ) {
+			$ManyToManyParser->init ();
+			$sqlExport = new SqlExport ();
+			$members = [ $ManyToManyParser->getFkField (),$ManyToManyParser->getMyFkField () ];
+			$fields = SqlUtils::getFieldList ( $members, $target );
+			$result [] = $sqlExport->exports ( $target, $fields );
 		}
-		return \implode("\n", $result);
+		return \implode ( "\n", $result );
 	}
 
-	public function addManyToMany($jointable,$memberTargetEntity){
-		if(!isset($this->manyToManys[$jointable])){
-			$this->manyToManys[$jointable]=new ManyToManyParser($memberTargetEntity["class"], $memberTargetEntity["member"]);
+	public function addManyToMany($jointable, $memberTargetEntity) {
+		if (! isset ( $this->manyToManys [$jointable] )) {
+			$this->manyToManys [$jointable] = new ManyToManyParser ( $memberTargetEntity ["class"], $memberTargetEntity ["member"] );
 		}
 	}
 }
