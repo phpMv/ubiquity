4391f8fc46be4e084451310e6242288944c35970
ndL-1990 <alamesch@vdl.lu>
2019-07-22 12:36:38 +0200

    Simplified the array for checks on the field name

--- src/Ubiquity/orm/creator/Model.php
+++ src/Ubiquity/orm/creator/Model.php
@@ -119,21 +119,13 @@ class Model {
 	private function getToStringField(){
 		$result=null;
 		// Array of multiple translations of the word "password" which could be taken as name of the table field in database
-		$pwArray=array('AmericanEnglish'=>'password','BrazilianPortuguese'=>'senha','Croatian'=>'lozinka','Czech'=>array('heslotajne','helslo_tajne'),'Danish'=>'password','Dutch'=>'wachtwoord','EuropeanSpanish'=>'contrasena','Finnish'=>'salasana','French'=>array('motdepasse','mot_de_passe'),'German'=>'passwort','Italian'=>'password','Norwegian'=>'passord','Polish'=>'haslo','EuropeanPortuguese'=>'senha','Romanian'=>'parola','Russian'=>'naponb','LatinAmericanSpanish'=>'contrasena','Swedish'=>array('loesenord','losenord'),'Turkish'=>'sifre','Ukrainian'=>'naponb','Vietnamese'=>array('matkhau','mat_khau'));
+		$pwArray=array('password','senha','lozinka','heslotajne','helslo_tajne','wachtwoord','contrasena','salasana','motdepasse','mot_de_passe','passwort','passord','haslo','senha','parola','naponb','contrasena','loesenord','losenord','sifre','naponb','matkhau','mat_khau');
 		foreach ( $this->members as $member ) {
 			if ($member->getDbType()!=="mixed" && $member->isNullable()!==true && !$member->isPrimary()) {
 			    $memberName=$member->getName();
-			    foreach ($pwArray as $key => $item) {
-			        if (is_array($item)) {
-                        if(!in_array($memberName,$item)) {
-                            $result=$member->getName();
-                        }
-                    } else {
-                        if($memberName!==$item) {
-                            $result=$memberName;
-                        }
-                    }
-                }
+			    if(!in_array($memberName,$pwArray)){
+			        $result=$memberName;
+			    }
 			}
 		}
 		return $result;
