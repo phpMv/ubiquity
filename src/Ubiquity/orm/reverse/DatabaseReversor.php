06d0c4da5e77c25ebc902df65c60d78ba72e9385
jcheron <myaddressmail@gmail.com>
2019-07-28 00:02:26 +0200

    Update DatabaseReversor.php

--- src/Ubiquity/orm/reverse/DatabaseReversor.php
+++ src/Ubiquity/orm/reverse/DatabaseReversor.php
@@ -6,26 +6,38 @@ use Ubiquity\db\reverse\DbGenerator;
 use Ubiquity\controllers\Startup;
 use Ubiquity\cache\CacheManager;
 
+/**
+ * Generates database from models.
+ * Ubiquity\orm\reverse$DatabaseReversor
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.1
+ *
+ */
 class DatabaseReversor {
 	private $generator;
-	public function __construct(DbGenerator $generator){
-		$this->generator=$generator;
+	private $database;
+
+	public function __construct(DbGenerator $generator, $databaseOffset = 'default') {
+		$this->generator = $generator;
+		$this->database = $databaseOffset;
 	}
 
-	public function createDatabase($name){
-		$this->generator->createDatabase($name);
-		$this->generator->selectDatabase($name);
-		$config=Startup::getConfig();
-		$models=CacheManager::getModels($config,true);
-		foreach ($models as $model){
-			$tableReversor=new TableReversor($model);
-			$tableReversor->initFromClass();
-			$tableReversor->generateSQL($this->generator);
+	public function createDatabase($name) {
+		$this->generator->createDatabase ( $name );
+		$this->generator->selectDatabase ( $name );
+		$config = Startup::getConfig ();
+		$models = CacheManager::getModels ( $config, true, $this->database );
+		foreach ( $models as $model ) {
+			$tableReversor = new TableReversor ( $model );
+			$tableReversor->initFromClass ();
+			$tableReversor->generateSQL ( $this->generator );
 		}
-		$this->generator->generateManyToManys();
+		$this->generator->generateManyToManys ();
 	}
 
-	public function __toString(){
-		return $this->generator->__toString();
+	public function __toString() {
+		return $this->generator->__toString ();
 	}
 }
