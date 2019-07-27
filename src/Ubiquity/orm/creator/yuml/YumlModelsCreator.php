28988adc27db8499d3cd4cee8b2cf0922776ac67
jcheron <myaddressmail@gmail.com>
2019-07-27 23:58:34 +0200

    Update YumlModelsCreator.php

--- src/Ubiquity/orm/creator/yuml/YumlModelsCreator.php
+++ src/Ubiquity/orm/creator/yuml/YumlModelsCreator.php
@@ -1,42 +1,51 @@
 <?php
+
 namespace Ubiquity\orm\creator\yuml;
 
 use Ubiquity\orm\creator\ModelsCreator;
 
-
-class YumlModelsCreator extends ModelsCreator{
+/**
+ * Generates models classes from Yuml.
+ * Ubiquity\orm\creator\yuml$YumlModelsCreator
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.1
+ *
+ */
+class YumlModelsCreator extends ModelsCreator {
 	/**
+	 *
 	 * @var YumlParser
 	 */
 	private $yumlParser;
 
-	protected function init($config){
-		parent::init($config);
+	protected function init($config, $offset = 'default') {
+		parent::init ( $config, $offset );
 	}
 
 	public function initYuml($yumlString) {
-		$this->yumlParser=new YumlParser($yumlString);
+		$this->yumlParser = new YumlParser ( $yumlString );
 	}
 
-
-	protected function getTablesName(){
-		return $this->yumlParser->getTableNames();
+	protected function getTablesName() {
+		return $this->yumlParser->getTableNames ();
 	}
 
 	protected function getFieldsInfos($tableName) {
-		$fieldsInfos=array();
-		$fields = $this->yumlParser->getFields($tableName);
-		foreach ($fields as $field) {
-			$fieldsInfos[$field['name']] = ["Type"=>$field['type'],"Nullable"=>(isset($field["null"]) && $field["null"])];
+		$fieldsInfos = array ();
+		$fields = $this->yumlParser->getFields ( $tableName );
+		foreach ( $fields as $field ) {
+			$fieldsInfos [$field ['name']] = [ "Type" => $field ['type'],"Nullable" => (isset ( $field ["null"] ) && $field ["null"]) ];
 		}
 		return $fieldsInfos;
 	}
 
-	protected function getPrimaryKeys($tableName){
-		return $this->yumlParser->getPrimaryKeys($tableName);
+	protected function getPrimaryKeys($tableName) {
+		return $this->yumlParser->getPrimaryKeys ( $tableName );
 	}
 
-	protected function getForeignKeys($tableName,$pkName){
-		return $this->yumlParser->getForeignKeys($tableName);
+	protected function getForeignKeys($tableName, $pkName) {
+		return $this->yumlParser->getForeignKeys ( $tableName );
 	}
 }
