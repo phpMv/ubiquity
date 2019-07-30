1576eab56062c485e37329cae5d45217305d4b49
jcheron <myaddressmail@gmail.com>
2019-07-30 10:41:34 +0200

    Update tests

--- src/tests/files/services.php
+++ src/tests/files/services.php
@@ -3,10 +3,7 @@ use Ubiquity\controllers\Router;
 $config ["debug"] = true;
 \Ubiquity\log\Logger::init ( $config );
 \Ubiquity\cache\CacheManager::startProd ( $config );
-$db = $config ["database"];
-if ($db ["dbName"] !== "") {
-	\Ubiquity\orm\DAO::connect ( $db ["type"], $db ["dbName"], @$db ["serverName"], @$db ["port"], @$db ["user"], @$db ["password"], @$db ["options"], @$db ["cache"] );
-}
+\Ubiquity\orm\DAO::start ();
 Router::startAll ();
 Router::addRoute ( "_default", "controllers\\IndexController" );
 \Ubiquity\assets\AssetsManager::start ( $config );
