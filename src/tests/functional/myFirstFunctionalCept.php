1d956ff437df0eb42b2155f6a6deb3d6e9b5e939
jcheron <myaddressmail@gmail.com>
2019-09-18 18:49:00 +0200

    Update myFirstFunctionalCept.php

--- src/tests/functional/myFirstFunctionalCept.php
+++ src/tests/functional/myFirstFunctionalCept.php
@@ -3,7 +3,3 @@ $I = new FunctionalTester ( $scenario );
 $I->wantTo ( 'perform actions and see result' );
 $I->amOnPage ( "/" );
 $I->see ( "Ubiquity" );
-$I->amOnPage ( "/Admin" );
-$I->see ( "UbiquityMyAdmin" );
-$I->amOnPage ( "/Admin/Models" );
-$I->see("Used to perform CRUD operations on data.","body");
