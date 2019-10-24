63cd478e0b099ad31b0ec35bbd7e0c8a46cb114b
jcheron <myaddressmail@gmail.com>
2019-10-24 03:37:21 +0200

    Add cache to MicroTemplateEngine

--- src/Ubiquity/views/View.php
+++ src/Ubiquity/views/View.php
@@ -2,85 +2,88 @@
 
 namespace Ubiquity\views;
 
-use Ubiquity\utils\base\UString;
 use Ubiquity\views\engine\TemplateEngine;
 use Ubiquity\controllers\Startup;
 
 /**
- * Represents a view
+ * Represents a view.
+ * Ubiquity\views$View
+ * This class is part of Ubiquity
+ *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.2
+ * @version 1.0.3
  *
  */
 class View {
 	private $vars;
 
 	public function __construct() {
-		$this->vars=array ();
+		$this->vars = array ();
 	}
 
 	protected function includeFileAsString($filename) {
-		\ob_start();
+		\ob_start ();
 		include ($filename);
-		return \ob_get_clean();
+		return \ob_get_clean ();
 	}
 
 	public function setVar($name, $value) {
-		$this->vars[$name]=$value;
+		$this->vars [$name] = $value;
 		return $this;
 	}
 
 	public function setVars($vars) {
-		if (\is_array($vars))
-			$this->vars=\array_merge($this->vars, $vars);
+		if (\is_array ( $vars ))
+			$this->vars = \array_merge ( $this->vars, $vars );
 		else
-			$this->vars=$vars;
+			$this->vars = $vars;
 		return $this;
 	}
 
 	public function getVar($name) {
-		if (\array_key_exists($name, $this->vars)) {
-			return $this->vars[$name];
+		if (\array_key_exists ( $name, $this->vars )) {
+			return $this->vars [$name];
 		}
 	}
 
 	/**
 	 * affiche la vue $viewName
+	 *
 	 * @param string $viewName nom de la vue à charger
 	 * @param boolean $asString Si vrai, la vue n'est pas affichée mais retournée sous forme de chaîne (utilisable dans une variable)
 	 * @throws \Exception
 	 * @return string
 	 */
-	public function render($viewName, $asString=false) {
-		$templateEngine=Startup::$templateEngine;
-		$ext=\pathinfo($viewName, PATHINFO_EXTENSION);
+	public function render($viewName, $asString = false) {
+		$templateEngine = Startup::$templateEngine;
+		$ext = \pathinfo ( $viewName, PATHINFO_EXTENSION );
 		if ($ext === null)
-			$viewName=$viewName . ".php";
-		$data=$this->vars;
-		if (!UString::endswith($viewName, ".php") && $templateEngine instanceof TemplateEngine) {
-			return $templateEngine->render($viewName, $data, $asString);
+			$viewName = $viewName . '.php';
+		$data = $this->vars;
+		if (\substr ( $viewName, - strlen ( '.php' ) ) !== '.php' && $templateEngine instanceof TemplateEngine) {
+			return $templateEngine->render ( $viewName, $data, $asString );
 		}
 
-		if (is_array($data)) {
-			extract($data);
+		if (\is_array ( $data )) {
+			\extract ( $data );
 		}
-		$fileName=\ROOT . \DS . "views".\DS . $viewName;
-		if(file_exists($fileName)){
+		$fileName = \ROOT . \DS . 'views' . \DS . $viewName;
+		if (\file_exists ( $fileName )) {
 			if ($asString) {
-				return $this->includeFileAsString($fileName);
+				return $this->includeFileAsString ( $fileName );
 			} else {
 				include ($fileName);
 			}
-		}else{
-			throw new \Exception("View {$viewName} not found!");
+		} else {
+			throw new \Exception ( "View {$viewName} not found!" );
 		}
 	}
-	
-	public function getBlockNames($templateName){
-		return Startup::$templateEngine->getBlockNames($templateName);
+
+	public function getBlockNames($templateName) {
+		return Startup::$templateEngine->getBlockNames ( $templateName );
 	}
-	
-	public function getCode($templateName){
-		return Startup::$templateEngine->getCode($templateName);
+
+	public function getCode($templateName) {
+		return Startup::$templateEngine->getCode ( $templateName );
 	}
 }
