874ae76a56a38446b7fe590db9e9d9b217d59c5c
jcheron <myaddressmail@gmail.com>
2019-11-18 19:27:40 +0100

    [skip ci] Fix php view loading pb

--- src/Ubiquity/views/View.php
+++ src/Ubiquity/views/View.php
@@ -11,7 +11,7 @@ use Ubiquity\controllers\Startup;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.3
+ * @version 1.0.4
  *
  */
 class View {
@@ -57,7 +57,7 @@ class View {
 	public function render($viewName, $asString = false) {
 		$templateEngine = Startup::$templateEngine;
 		$ext = \pathinfo ( $viewName, PATHINFO_EXTENSION );
-		if ($ext === null)
+		if ($ext == null)
 			$viewName = $viewName . '.php';
 		$data = $this->vars;
 		if (\substr ( $viewName, - strlen ( '.php' ) ) !== '.php' && $templateEngine instanceof TemplateEngine) {
