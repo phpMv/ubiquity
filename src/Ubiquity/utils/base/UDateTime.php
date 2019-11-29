9cc0a2e0a40f6de80ad002558c56432cc9903185
jcheron <myaddressmail@gmail.com>
2019-11-29 02:30:16 +0100

    Update UArray(DateTime) & UDateTime (elapse)

--- src/Ubiquity/utils/base/UDateTime.php
+++ src/Ubiquity/utils/base/UDateTime.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ubiquity\utils\base;
 
 /**
@@ -12,52 +11,66 @@ namespace Ubiquity\utils\base;
  *
  */
 class UDateTime {
+
 	const MYSQL_DATETIME_FORMAT = 'Y-m-d H:i:s';
+
 	const MYSQL_DATE_FORMAT = 'Y-m-d';
-	private static $locales = [ "fr" => [ "fr","fr_FR","fr_FR.UTF-8" ],"en" => [ "en","en_US","en_US.UTF-8" ] ];
+
+	private static $locales = [
+		"fr" => [
+			"fr",
+			"fr_FR",
+			"fr_FR.UTF-8"
+		],
+		"en" => [
+			"en",
+			"en_US",
+			"en_US.UTF-8"
+		]
+	];
 
 	private static function setLocale($locale) {
-		if (isset ( self::$locales [$locale] )) {
-			$locale = self::$locales [$locale];
+		if (isset(self::$locales[$locale])) {
+			$locale = self::$locales[$locale];
 		} else {
-			$locale = self::$locales ["en"];
+			$locale = self::$locales["en"];
 		}
-		setlocale ( LC_TIME, $locale [0], $locale [1], $locale [2] );
+		setlocale(LC_TIME, $locale[0], $locale[1], $locale[2]);
 	}
 
 	public static function secondsToTime($seconds) {
-		$hours = floor ( $seconds / 3600 );
-		$mins = floor ( $seconds / 60 % 60 );
-		$secs = floor ( $seconds % 60 );
-		return sprintf ( '%02d:%02d:%02d', $hours, $mins, $secs );
+		$hours = floor($seconds / 3600);
+		$mins = floor($seconds / 60 % 60);
+		$secs = floor($seconds % 60);
+		return sprintf('%02d:%02d:%02d', $hours, $mins, $secs);
 	}
 
 	public static function mysqlDate(\DateTime $date) {
-		return $date->format ( self::MYSQL_DATE_FORMAT );
+		return $date->format(self::MYSQL_DATE_FORMAT);
 	}
 
 	public static function mysqlDateTime(\DateTime $datetime) {
-		return $datetime->format ( self::MYSQL_DATETIME_FORMAT );
+		return $datetime->format(self::MYSQL_DATETIME_FORMAT);
 	}
 
 	public static function longDate($date, $locale = "en") {
-		self::setLocale ( $locale );
-		return utf8_encode ( strftime ( "%A %d %B %Y", strtotime ( $date ) ) );
+		self::setLocale($locale);
+		return utf8_encode(strftime("%A %d %B %Y", strtotime($date)));
 	}
 
 	public static function shortDate($date, $locale = "en") {
-		self::setLocale ( $locale );
-		return strftime ( "%x", strtotime ( $date ) );
+		self::setLocale($locale);
+		return strftime("%x", strtotime($date));
 	}
 
 	public static function shortDatetime($datetime, $locale = "en") {
-		self::setLocale ( $locale );
-		return strftime ( "%c", strtotime ( $datetime ) );
+		self::setLocale($locale);
+		return strftime("%c", strtotime($datetime));
 	}
 
 	public static function longDatetime($datetime, $locale = "en") {
-		self::setLocale ( $locale );
-		return utf8_encode ( strftime ( "%A %d %B %Y, %T", strtotime ( $datetime ) ) );
+		self::setLocale($locale);
+		return utf8_encode(strftime("%A %d %B %Y, %T", strtotime($datetime)));
 	}
 
 	/**
@@ -68,25 +81,37 @@ class UDateTime {
 	 * @see http://stackoverflow.com/questions/1416697/converting-timestamp-to-time-ago-in-php-e-g-1-day-ago-2-days-ago
 	 */
 	public static function elapsed($datetime, $full = false) {
-		$now = new \DateTime ();
-		$ago = new \DateTime ( $datetime );
-		$diff = $now->diff ( $ago );
+		$now = new \DateTime();
+		if (! $datetime instanceof \DateTime) {
+			$ago = new \DateTime($datetime);
+		} else {
+			$ago = $datetime;
+		}
+		$diff = $now->diff($ago);
 
-		$diff->w = floor ( $diff->d / 7 );
+		$diff->w = floor($diff->d / 7);
 		$diff->d -= $diff->w * 7;
 
-		$string = array ('y' => 'year','m' => 'month','w' => 'week','d' => 'day','h' => 'hour','i' => 'minute','s' => 'second' );
-		foreach ( $string as $k => &$v ) {
+		$string = array(
+			'y' => 'year',
+			'm' => 'month',
+			'w' => 'week',
+			'd' => 'day',
+			'h' => 'hour',
+			'i' => 'minute',
+			's' => 'second'
+		);
+		foreach ($string as $k => &$v) {
 			if ($diff->$k) {
 				$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
 			} else {
-				unset ( $string [$k] );
+				unset($string[$k]);
 			}
 		}
 
 		if (! $full)
-			$string = array_slice ( $string, 0, 1 );
-		return $string ? implode ( ', ', $string ) . ' ago' : 'just now';
+			$string = array_slice($string, 0, 1);
+		return $string ? implode(', ', $string) . ($diff->invert ? ' ago' : '') : 'just now';
 	}
 }
 
