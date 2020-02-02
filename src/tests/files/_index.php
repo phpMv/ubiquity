372b3a466691af9056ac784e4a516e66842e7900
jcheron <myaddressmail@gmail.com>
2020-02-03 00:06:42 +0100

    Fix tests

--- src/tests/files/_index.php
+++ src/tests/files/_index.php
@@ -11,6 +11,13 @@ $config ["siteUrl"] = "http://dev.local/";
 $config ['sessionName'] = '';
 require ROOT . './../vendor/autoload.php';
 require ROOT . 'config/services.php';
+if (\class_exists ( "\\Monolog\\Logger" )) {
+	$config ['logger'] = function () use ($config) {
+		return new \Ubiquity\log\libraries\UMonolog ( $config ['sessionName'], \Monolog\Logger::INFO );
+	};
+	$config ['debug'] = true;
+	\Ubiquity\log\Logger::init ( $config );
+}
 \Ubiquity\controllers\Router::get ( "c3/(.*?)", function () {
 	require ROOT . './../c3.php';
 } );
