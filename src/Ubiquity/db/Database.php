f6bff437f372366e9d28951bfdbbc5c1541b51df
jcheron <myaddressmail@gmail.com>
2020-02-06 01:54:50 +0100

    [skip ci] add default value to quote

--- src/Ubiquity/db/Database.php
+++ src/Ubiquity/db/Database.php
@@ -53,7 +53,7 @@ class Database {
 	 * @param boolean|string $cache
 	 * @param mixed $pool
 	 */
-	public function __construct($dbWrapperClass, $dbType, $dbName, $serverName = "127.0.0.1", $port = "3306", $user = "root", $password = "", $options = [], $cache = false, $pool = null) {
+	public function __construct($dbWrapperClass, $dbType, $dbName, $serverName = "127.0.0.1", $port = "3306", $user = "root", $password = "", $options = [ ], $cache = false, $pool = null) {
 		$this->setDbWrapperClass ( $dbWrapperClass, $dbType );
 		$this->dbName = $dbName;
 		$this->serverName = $serverName;
@@ -242,7 +242,7 @@ class Database {
 		return null;
 	}
 
-	public function quoteValue($value, $type = null) {
+	public function quoteValue($value, $type = 2) {
 		return $this->wrapperObject->quoteValue ( $value, $type );
 	}
 
