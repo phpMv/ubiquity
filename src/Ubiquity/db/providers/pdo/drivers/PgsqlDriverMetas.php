9b8172a840e7b01025dc535b041d57bd62a6a0dc
jcheron <myaddressmail@gmail.com>
2020-02-10 10:54:56 +0100

    Add todo comments

--- src/Ubiquity/db/providers/pdo/drivers/PgsqlDriverMetas.php
+++ src/Ubiquity/db/providers/pdo/drivers/PgsqlDriverMetas.php
@@ -13,15 +13,19 @@ namespace Ubiquity\db\providers\pdo\drivers;
 class PgsqlDriverMetas extends AbstractDriverMetaDatas {
 
 	public function getForeignKeys($tableName, $pkName, $dbName = null): array {
+		// TODO
 	}
 
 	public function getTablesName(): array {
+		// TODO
 	}
 
 	public function getPrimaryKeys($tableName): array {
+		// TODO
 	}
 
 	public function getFieldsInfos($tableName): array {
+		// TODO
 	}
 }
 
