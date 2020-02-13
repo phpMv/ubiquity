68d52efba91ddfb9c6e0e8acfd2b995ac6fb383e
US <us@nomail.com>
2020-02-13 22:34:10 +0300

    SQLite PDO Driver Added
    SQLite PDO Driver Added

--- src/Ubiquity/db/providers/pdo/drivers/SqliteDriverMetas.php
+++ src/Ubiquity/db/providers/pdo/drivers/SqliteDriverMetas.php
@@ -13,19 +13,35 @@ namespace Ubiquity\db\providers\pdo\drivers;
 class SqliteDriverMetas extends AbstractDriverMetaDatas {
 
 	public function getForeignKeys($tableName, $pkName, $dbName = null): array {
-		// TODO
+		$recordset = $this->dbInstance->query ( "SELECT \"TABLE\" as TABLE_NAME , \"to\" as COLUMN_NAME , \"from\" as REFERENCED_TABLE_SCHEMA FROM pragma_foreign_key_list('".$tableName."') WHERE \"to\"='".$pkName."';");
+		return $recordset->fetchAll ( \PDO::FETCH_ASSOC );
 	}
 
 	public function getTablesName(): array {
-		// TODO
+		$query = $this->dbInstance->query ( 'SELECT name FROM sqlite_master WHERE type=\'table\';'  );
+		return $query->fetchAll ( \PDO::FETCH_COLUMN );
 	}
 
 	public function getPrimaryKeys($tableName): array {
-		// TODO
+		$fieldkeys = array ();
+		$recordset = $this->dbInstance->query ( "PRAGMA TABLE_INFO(".$table_name.");" );
+		$keys = $recordset->fetchAll ( \PDO::FETCH_ASSOC );
+		foreach ( $keys as $key ) {
+            if ($key['pk']==1) {
+                $fieldkeys[] = $key['name'];
+            }			
+		}
+		return $fieldkeys;
 	}
 
 	public function getFieldsInfos($tableName): array {
-		// TODO
+		$fieldsInfos = array ();
+		$recordset = $this->dbInstance->query (  "PRAGMA TABLE_INFO(".$table_name.");" );
+		$fields = $recordset->fetchAll ( \PDO::FETCH_ASSOC );
+		foreach ( $fields as $field ) {
+			$fieldsInfos [$field ['name']] = [ "Type" => $field ['type'],"Nullable" => $field ["notnull"] ];
+		}
+		return $fieldsInfos;
 	}
 }
 
