85da691563dd5d928924d42e7cd246517d019aeb
US <us@nomail.com>
2020-02-14 15:52:56 +0300

    SQLite getRowNum added
    SQLite getRowNum added

--- src/Ubiquity/db/providers/pdo/drivers/SqliteDriverMetas.php
+++ src/Ubiquity/db/providers/pdo/drivers/SqliteDriverMetas.php
@@ -12,9 +12,10 @@ namespace Ubiquity\db\providers\pdo\drivers;
  */
 class SqliteDriverMetas extends AbstractDriverMetaDatas {
 
+
 	public function getForeignKeys($tableName, $pkName, $dbName = null): array {
 
-		// SQL lite may return error if key is not found.
+		// SQL lite may return error if key is not found when manual model editing can cause it by mistype of model field name.
 		$recordset = $this->dbInstance->query ( "SELECT \"TABLE\" as TABLE_NAME , \"to\" as COLUMN_NAME , \"from\" as REFERENCED_TABLE_SCHEMA FROM pragma_foreign_key_list('".$tableName."') WHERE \"to\"='".$pkName."';");
 		return $recordset->fetchAll ( \PDO::FETCH_ASSOC );
 	}
@@ -44,6 +45,14 @@ class SqliteDriverMetas extends AbstractDriverMetaDatas {
 			$fieldsInfos [$field ['name']] = [ "Type" => $field ['type'],"Nullable" => $field ["notnull"] ];
 		}
 		return $fieldsInfos;
+	}
+	public function getRowNum(string $tableName, string $pkName, string $condition): int
+	{
+		$query = $this->dbInstance->query ( "SELECT num FROM (SELECT *,row_number() OVER (ORDER BY {$pkName}) AS num FROM \"{$tableName}\") x where " . $condition );
+		if ($query) {
+			return $query->fetchColumn ( 0 );
+		}
+		return 0;
 	}
 }
 
