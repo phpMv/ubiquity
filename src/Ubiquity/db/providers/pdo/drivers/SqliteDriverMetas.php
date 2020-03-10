3ae514940024c2f070a5a71541041c943170609f
jcheron <myaddressmail@gmail.com>
2020-03-10 01:14:51 +0100

    Add SQLite pdo driver Support (#101) (#105)
    * [skip ci] update CHANGELOG
    
    * SQLite Pdo Driver and Minor update
    
    SQLite Pdo Driver and Minor update
    
    * Delete SQLite PDO Driver to Create New PR
    
    Delete SQLite PDO Driver to Create New PR
    
    * Added New SQLite PDO Driver
    
    Added New SQLite PDO Driver
    
    * Revert "Added New SQLite PDO Driver"
    
    This reverts commit 379b84df2867e01d7a49170bc6550c7adce8fbd9.
    
    * Revert "Delete SQLite PDO Driver to Create New PR"
    
    This reverts commit 05fd655ad5304e6ce2fe6dc183495f62f230d89d.
    
    * Revert "Added New SQLite PDO Driver"
    
    This reverts commit 379b84df2867e01d7a49170bc6550c7adce8fbd9.
    
    * Revert "Delete SQLite PDO Driver to Create New PR"
    
    This reverts commit 05fd655ad5304e6ce2fe6dc183495f62f230d89d.
    
    * Revert "SQLite Pdo Driver and Minor update"
    
    This reverts commit d119f5f9ab7d584d1e5ad2dc48fe6f262c4bbf79.
    
    * minor Update for PostgreSQL PDO
    
    minor update for default value
    
    * SQLite PDO Driver Added
    
    SQLite PDO Driver Added
    
    * Revert "SQLite PDO Driver Added"
    
    This reverts commit 68d52efba91ddfb9c6e0e8acfd2b995ac6fb383e.
    
    * Update SqliteDriverMetas
    
    Update SqliteDriverMetas
    
    * Delete PostGreSQL PDO
    
    DElete PostGRESQL PDO
    
    * Revert "Delete PostGreSQL PDO"
    
    This reverts commit 8f5d10836ea2a5a846e1c982da0b2dcca72fefaa.
    
    * SQLite getRowNum added
    
    SQLite getRowNum added
    
    Co-authored-by: jcheron <myaddressmail@gmail.com>
    Co-authored-by: US <us@nomail.com>
    
    Co-authored-by: Ulaş SAYGIN <ulassayginim@gmail.com>
    Co-authored-by: US <us@nomail.com>

--- src/Ubiquity/db/providers/pdo/drivers/SqliteDriverMetas.php
+++ src/Ubiquity/db/providers/pdo/drivers/SqliteDriverMetas.php
@@ -12,20 +12,47 @@ namespace Ubiquity\db\providers\pdo\drivers;
  */
 class SqliteDriverMetas extends AbstractDriverMetaDatas {
 
+
 	public function getForeignKeys($tableName, $pkName, $dbName = null): array {
-		// TODO
+
+		// SQL lite may return error if key is not found when manual model editing can cause it by mistype of model field name.
+		$recordset = $this->dbInstance->query ( "SELECT \"TABLE\" as TABLE_NAME , \"to\" as COLUMN_NAME , \"from\" as REFERENCED_TABLE_SCHEMA FROM pragma_foreign_key_list('".$tableName."') WHERE \"to\"='".$pkName."';");
+		return $recordset->fetchAll ( \PDO::FETCH_ASSOC );
 	}
 
 	public function getTablesName(): array {
-		// TODO
+		$query = $this->dbInstance->query ( 'SELECT name FROM sqlite_master WHERE type=\'table\';'  );
+		return $query->fetchAll ( \PDO::FETCH_COLUMN );
 	}
 
 	public function getPrimaryKeys($tableName): array {
-		// TODO
+		$fieldkeys = array ();
+		$recordset = $this->dbInstance->query ( "PRAGMA TABLE_INFO(".$table_name.");" );
+		$keys = $recordset->fetchAll ( \PDO::FETCH_ASSOC );
+		foreach ( $keys as $key ) {
+            if ($key['pk']==1) {
+                $fieldkeys[] = $key['name'];
+            }			
+		}
+		return $fieldkeys;
 	}
 
 	public function getFieldsInfos($tableName): array {
-		// TODO
+		$fieldsInfos = array ();
+		$recordset = $this->dbInstance->query (  "PRAGMA TABLE_INFO(".$table_name.");" );
+		$fields = $recordset->fetchAll ( \PDO::FETCH_ASSOC );
+		foreach ( $fields as $field ) {
+			$fieldsInfos [$field ['name']] = [ "Type" => $field ['type'],"Nullable" => $field ["notnull"] ];
+		}
+		return $fieldsInfos;
+	}
+	public function getRowNum(string $tableName, string $pkName, string $condition): int
+	{
+		$query = $this->dbInstance->query ( "SELECT num FROM (SELECT *,row_number() OVER (ORDER BY {$pkName}) AS num FROM \"{$tableName}\") x where " . $condition );
+		if ($query) {
+			return $query->fetchColumn ( 0 );
+		}
+		return 0;
 	}
 }
 
