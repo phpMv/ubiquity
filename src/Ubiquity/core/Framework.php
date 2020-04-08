8134396096562dd8c02a5abe84e65cc7015f8065
jcheron <myaddressmail@gmail.com>
2020-04-08 13:35:36 +0200

    [skip ci] Update version to 2.3.5

--- src/Ubiquity/core/Framework.php
+++ src/Ubiquity/core/Framework.php
@@ -21,7 +21,7 @@ use Ubiquity\utils\http\URequest;
 use Ubiquity\utils\http\USession;
 
 class Framework {
-	public const version = '2.3.4';
+	public const version = '2.3.5';
 
 	public static function getVersion() {
 		return self::version;
