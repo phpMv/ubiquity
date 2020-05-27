79cdc2436134f79a693fd25618cd5e52318ccb8d
jcheron <myaddressmail@gmail.com>
2020-05-27 04:05:41 +0200

    [skip ci] create NoCsrfProtection class (session without Csrf protection)

---
+++ src/Ubiquity/utils/http/session/protection/NoCsrfProtection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ubiquity\utils\http\session\protection;
+
+/**
+ * To be Used only if no Csrf protection of the session is required.
+ * with Startup::setSessionInstance(new PhpSession(new NoCsrfProtection())); in services.php
+ * Ubiquity\utils\http\session\protection$NoCsrfProtection
+ * This class is part of Ubiquity
+ *
+ * @author jc
+ * @version 1.0.0
+ *
+ */
+class NoCsrfProtection implements VerifySessionCsrfInterface {
+
+	public function init() {
+	}
+
+	public function start() {
+	}
+
+	public function clear() {
+	}
+}
