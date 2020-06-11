60f0f29d5a2bc712ab134fdef1ec36510ad8bcf6
jcheron <myaddressmail@gmail.com>
2020-06-12 01:08:31 +0200

    Add crypt transformer

--- src/Ubiquity/contents/transformation/TransformersManager.php
+++ src/Ubiquity/contents/transformation/TransformersManager.php
@@ -20,7 +20,7 @@ use Ubiquity\utils\base\UArray;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.1
+ * @version 1.0.2
  *
  */
 class TransformersManager {
@@ -30,63 +30,75 @@ class TransformersManager {
 	 * @var array|mixed
 	 */
 	private static $transformers = [ 'md5' => Md5::class,'datetime' => DateTime::class,'upper' => UpperCase::class,'firstUpper' => FirstUpperCase::class,'lower' => LowerCase::class,'password' => Password::class ];
-	private static $key = "contents/transformers";
+	private static $key = 'contents/transformers';
 
 	/**
+	 * Load all transformers.
 	 * Do not use at runtime !
 	 */
 	public static function start() {
-		$transformers = [ ];
 		if (CacheManager::$cache->exists ( self::$key )) {
-			$transformers = CacheManager::$cache->fetch ( self::$key );
+			self::$transformers = \array_merge ( self::$transformers, CacheManager::$cache->fetch ( self::$key ) );
 		}
-		self::$transformers = array_merge ( self::$transformers, $transformers );
-	}
-
-	public static function startProd($op = null) {
-		DAO::$useTransformers = true;
-		DAO::$transformerOp = $op ?? 'transform';
+		if (\class_exists ( '\\Ubiquity\\security\\csrf\\CsrfManager' )) {
+			self::$transformers ['crypt'] = '\\Ubiquity\\contents\\transformation\\transformers\\Crypt';
+		}
 	}
 
 	/**
+	 * Start the manager for production.
+	 *
+	 * @param ?string $op
+	 */
+	public static function startProd(?string $op = 'transform'): void {
+		self::start ();
+		DAO::$useTransformers = true;
+		DAO::$transformerOp = $op;
+	}
+
+	/**
+	 * Register a new transformer class.
 	 * Do not used at runtime !
 	 *
 	 * @param string $transformer
 	 * @param string $classname
 	 */
-	public static function registerClass($transformer, $classname) {
+	public static function registerClass(string $transformer, string $classname): void {
 		self::$transformers [$transformer] = $classname;
 	}
 
 	/**
+	 * Register and save in cache a new Transformer.
 	 * Do not used at runtime !
 	 *
 	 * @param string $transformer
 	 * @param string $classname
 	 */
-	public static function registerClassAndSave($transformer, $classname) {
+	public static function registerClassAndSave(string $transformer, string $classname): void {
 		self::start ();
 		self::registerClass ( $transformer, $classname );
 		self::store ();
 	}
 
 	/**
+	 * Register an associative array of transformers based on ['name'=>'transformerClass'].
 	 * Do not used at runtime !
 	 *
 	 * @param array $transformersAndClasses
 	 */
-	public static function registerClasses($transformersAndClasses) {
+	public static function registerClasses(array $transformersAndClasses): void {
 		foreach ( $transformersAndClasses as $transformer => $class ) {
 			self::registerClass ( $transformer, $class );
 		}
 	}
 
 	/**
+	 * Register and save an associative array of transformers based on ['name'=>'transformerClass'].
 	 * Do not used at runtime !
 	 *
 	 * @param array $transformersAndClasses
 	 */
-	public static function registerClassesAndSave($transformersAndClasses) {
+	public static function registerClassesAndSave(array $transformersAndClasses): void {
 		self::start ();
 		foreach ( $transformersAndClasses as $transformer => $class ) {
 			self::registerClass ( $transformer, $class );
@@ -94,23 +106,46 @@ class TransformersManager {
 		self::store ();
 	}
 
-	public static function getTransformerClass($transformer) {
+	/**
+	 * Return the class from a transformer name.
+	 *
+	 * @param string $transformer
+	 * @return ?string
+	 */
+	public static function getTransformerClass(string $transformer): ?string {
 		if (isset ( self::$transformers [$transformer] )) {
 			return self::$transformers [$transformer];
 		}
 		return null;
 	}
 
-	public static function transform($instance, $member, $transform = 'transform') {
+	/**
+	 * Transform a member of an instance.
+	 *
+	 * @param object $instance
+	 * @param string $member
+	 * @param string $transform
+	 * @return ?mixed
+	 */
+	public static function transform(object $instance, string $member, ?string $transform = 'transform') {
 		$getter = 'get' . $member;
-		if (method_exists ( $instance, $getter )) {
+		if (\method_exists ( $instance, $getter )) {
 			return self::applyTransformer ( $instance, $member, $instance->{$getter} (), $transform );
 		}
 		return null;
 	}
 
-	public static function applyTransformer($instance, $member, $value, $transform = 'transform') {
-		$class = get_class ( $instance );
+	/**
+	 * Apply a transformer using a member transformer(s) on a value.
+	 *
+	 * @param object $instance
+	 * @param string $member
+	 * @param mixed $value
+	 * @param string $transform
+	 * @return mixed
+	 */
+	public static function applyTransformer(object $instance, string $member, $value, ?string $transform = 'transform') {
+		$class = \get_class ( $instance );
 		$metas = OrmUtils::getModelMetadata ( $class );
 		if (isset ( $metas ['#transformers'] [$transform] [$member] )) {
 			$transformer = $metas ['#transformers'] [$transform] [$member];
@@ -119,16 +154,22 @@ class TransformersManager {
 		return $value;
 	}
 
-	public static function transformInstance($instance, $transform = 'transform') {
-		$class = get_class ( $instance );
+	/**
+	 * Transform all the members of a model instance.
+	 *
+	 * @param object $instance
+	 * @param string $transform
+	 */
+	public static function transformInstance(object $instance, $transform = 'transform'): void {
+		$class = \get_class ( $instance );
 		$metas = OrmUtils::getModelMetadata ( $class );
 		$transformers = $metas ['#transformers'] [$transform] ?? [ ];
 		foreach ( $transformers as $member => $transformer ) {
 			$getter = 'get' . ucfirst ( $member );
 			$setter = 'set' . ucfirst ( $member );
-			if (method_exists ( $instance, $getter )) {
+			if (\method_exists ( $instance, $getter )) {
 				$value = $transformer::$transform ( $instance->{$getter} () );
-				if (method_exists ( $instance, $setter )) {
+				if (\method_exists ( $instance, $setter )) {
 					$instance->{$setter} ( $value );
 				}
 				$instance->_rest [$member] = $value;
@@ -136,7 +177,10 @@ class TransformersManager {
 		}
 	}
 
-	public static function store() {
+	/**
+	 * Store the loaded transformers in cache.
+	 */
+	public static function store(): void {
 		CacheManager::$cache->store ( self::$key, "return " . UArray::asPhpArray ( self::$transformers, 'array' ) . ';' );
 	}
 }
