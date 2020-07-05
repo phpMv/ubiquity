71ccf10f01a0e439bc509f90536c49650a790053
jcheron <myaddressmail@gmail.com>
2020-07-05 18:30:08 +0200

    [skip ci] add MemoryCache for Db

---
+++ src/Ubiquity/cache/database/MemoryCache.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ubiquity\cache\database;
+
+use Ubiquity\cache\database\traits\MemoryCacheTrait;
+
+/**
+ * Ubiquity\cache\database$MemoryCache
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.0
+ *
+ */
+class MemoryCache extends DbCache {
+	use MemoryCacheTrait;
+
+	protected function getMemoryKey($tableName, $condition) {
+		return $tableName . '.' . $this->getKey ( $condition );
+	}
+
+	public function fetch($tableName, $condition) {
+		$key = $this->getMemoryKey ( $tableName, $condition );
+		if (isset ( $this->memoryCache [$key] )) {
+			return $this->memoryCache [$key];
+		}
+		return false;
+	}
+
+	public function store($tableName, $condition, $result) {
+		$this->memoryCache [$this->getMemoryKey ( $tableName, $condition )] = $result;
+	}
+
+	public function delete($tableName, $condition) {
+		$key = $this->getMemoryKey ( $tableName, $condition );
+		if (isset ( $this->memoryCache [$key] )) {
+			unset ( $this->memoryCache [$key] );
+			return true;
+		}
+		return false;
+	}
+}
