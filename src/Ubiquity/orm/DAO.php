868a20d051c3fd1ee7d56c9a68801d6e889370c9
jcheron <myaddressmail@gmail.com>
2020-07-09 03:06:59 +0200

    [skip ci] Fix DAO cache warmup

--- src/Ubiquity/orm/DAO.php
+++ src/Ubiquity/orm/DAO.php
@@ -323,8 +323,6 @@ class DAO {
 			self::$cache->store ( $className, OrmUtils::getKeyValues ( $o ), $o );
 		}
 		$offset = self::$modelsDatabase [$className] ?? 'default';
-		$db = self::$db [$offset];
-		$db->close ();
 		unset ( self::$db [$offset] );
 	}
 
