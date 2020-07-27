6343d6c8f46393efd939bd59012d20584087794f
jcheron <myaddressmail@gmail.com>
2020-07-28 01:23:32 +0200

    [skip ci] update version to 2.3.11

--- src/Ubiquity/core/Framework.php
+++ src/Ubiquity/core/Framework.php
@@ -20,7 +20,7 @@ use Ubiquity\utils\http\URequest;
 use Ubiquity\utils\http\USession;
 
 class Framework {
-	public const version = '2.3.10';
+	public const version = '2.3.11';
 
 	public static function getVersion() {
 		return self::version;
