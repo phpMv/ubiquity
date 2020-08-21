16598fbbe4c34b61bc05fb1e65a96800c068eb3a
jcheron <myaddressmail@gmail.com>
2020-08-21 20:07:57 +0200

    Update AbstractDatabase.php

--- src/Ubiquity/db/AbstractDatabase.php
+++ src/Ubiquity/db/AbstractDatabase.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ubiquity\db;
 
 use Ubiquity\cache\database\DbCache;
@@ -7,13 +6,23 @@ use Ubiquity\exceptions\CacheException;
 use Ubiquity\exceptions\DBException;
 
 class AbstractDatabase {
+
+	public static $wrappers;
+
 	protected $dbType;
+
 	protected $serverName;
+
 	protected $port;
+
 	protected $dbName;
+
 	protected $user;
+
 	protected $password;
+
 	protected $cache;
+
 	protected $options;
 
 	/**
@@ -35,8 +44,8 @@ class AbstractDatabase {
 	 * @param boolean|string $cache
 	 * @param mixed $pool
 	 */
-	public function __construct($dbWrapperClass, $dbType, $dbName, $serverName = "127.0.0.1", $port = "3306", $user = "root", $password = "", $options = [ ], $cache = false, $pool = null) {
-		$this->setDbWrapperClass ( $dbWrapperClass, $dbType );
+	public function __construct($dbWrapperClass, $dbType, $dbName, $serverName = "127.0.0.1", $port = "3306", $user = "root", $password = "", $options = [], $cache = false, $pool = null) {
+		$this->setDbWrapperClass($dbWrapperClass, $dbType);
 		$this->dbName = $dbName;
 		$this->serverName = $serverName;
 		$this->port = $port;
@@ -45,22 +54,22 @@ class AbstractDatabase {
 		$this->options = $options;
 		if ($cache !== false) {
 			if ($cache instanceof \Closure) {
-				$this->cache = $cache ();
+				$this->cache = $cache();
 			} else {
-				if (\class_exists ( $cache )) {
-					$this->cache = new $cache ();
+				if (\class_exists($cache)) {
+					$this->cache = new $cache();
 				} else {
-					throw new CacheException ( $cache . " is not a valid value for database cache" );
+					throw new CacheException($cache . " is not a valid value for database cache");
 				}
 			}
 		}
-		if ($pool && (\method_exists ( $this->wrapperObject, 'pool' ))) {
-			$this->wrapperObject->setPool ( $pool );
+		if ($pool && (\method_exists($this->wrapperObject, 'pool'))) {
+			$this->wrapperObject->setPool($pool);
 		}
 	}
 
 	protected function setDbWrapperClass($dbWrapperClass, $dbType) {
-		$this->wrapperObject = new $dbWrapperClass ( $this->dbType = $dbType );
+		$this->wrapperObject = new $dbWrapperClass($this->dbType = $dbType);
 	}
 
 	/**
@@ -71,15 +80,15 @@ class AbstractDatabase {
 	 */
 	public function connect() {
 		try {
-			$this->_connect ();
+			$this->_connect();
 			return true;
-		} catch ( \Exception $e ) {
-			throw new DBException ( $e->getMessage (), $e->getCode (), $e->getPrevious () );
+		} catch (\Exception $e) {
+			throw new DBException($e->getMessage(), $e->getCode(), $e->getPrevious());
 		}
 	}
 
 	public function getDSN() {
-		return $this->wrapperObject->getDSN ( $this->serverName, $this->port, $this->dbName, $this->dbType );
+		return $this->wrapperObject->getDSN($this->serverName, $this->port, $this->dbName, $this->dbType);
 	}
 
 	/**
@@ -128,7 +137,7 @@ class AbstractDatabase {
 	}
 
 	public static function getAvailableDrivers($dbWrapperClass = \Ubiquity\db\providers\pdo\PDOWrapper::class) {
-		return \call_user_func ( $dbWrapperClass . '::getAvailableDrivers' );
+		return \call_user_func($dbWrapperClass . '::getAvailableDrivers');
 	}
 
 	/**
@@ -202,32 +211,32 @@ class AbstractDatabase {
 	 * Closes the active connection
 	 */
 	public function close() {
-		$this->wrapperObject->close ();
+		$this->wrapperObject->close();
 	}
 
 	/**
 	 * For databases with Connection pool (put a dbInstance in pool wrapper)
 	 */
 	public function freePool($db) {
-		$this->wrapperObject->freePool ( $db );
+		$this->wrapperObject->freePool($db);
 	}
 
 	public function setPool($pool) {
-		$this->wrapperObject->setPool ( $pool );
+		$this->wrapperObject->setPool($pool);
 	}
 
 	/**
 	 * For databases with Connection pool (retrieve a new dbInstance from pool wrapper)
 	 */
 	public function pool() {
-		return $this->wrapperObject->pool ();
+		return $this->wrapperObject->pool();
 	}
 
 	public static function getAvailableWrappers() {
-		$wrappers = [ ];
-		foreach ( self::$wrappers as $k => $wrapper ) {
-			if (\class_exists ( $wrapper, true )) {
-				$wrappers [$k] = $wrapper;
+		$wrappers = [];
+		foreach (static::$wrappers as $k => $wrapper) {
+			if (\class_exists($wrapper, true)) {
+				$wrappers[$k] = $wrapper;
 			}
 		}
 		return $wrappers;
