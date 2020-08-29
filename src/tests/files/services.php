edc8e83181ea147903d772e178f0d7c32184a10f
jcheron <myaddressmail@gmail.com>
2020-08-29 16:50:12 +0200

    [ci-tests] fix typo

--- src/tests/files/services.php
+++ src/tests/files/services.php
@@ -3,7 +3,7 @@ use Ubiquity\controllers\Router;
 $config ["debug"] = true;
 \Ubiquity\log\Logger::init ( $config );
 \Ubiquity\cache\CacheManager::startProd ( $config );
-\Ubiquity\orm\DAO::setModelsDatabases ( $modelsDatabase );
+\Ubiquity\orm\DAO::start ();
 Router::startAll ();
 Router::addRoute ( "_default", "controllers\\IndexController" );
 \Ubiquity\assets\AssetsManager::start ( $config );
