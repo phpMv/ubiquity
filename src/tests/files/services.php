e24edc949fb0bb48081b9c7468a75ab11575e7e4
jcheron <myaddressmail@gmail.com>
2020-08-29 16:11:09 +0200

    [ci-tests] add models creation for bench offset

--- src/tests/files/services.php
+++ src/tests/files/services.php
@@ -3,7 +3,7 @@ use Ubiquity\controllers\Router;
 $config ["debug"] = true;
 \Ubiquity\log\Logger::init ( $config );
 \Ubiquity\cache\CacheManager::startProd ( $config );
-\Ubiquity\orm\DAO::setModelsDatabases ( [ "models\\bench\\Fortune" => "bench","models\\bench\\World" => "bench","models\\Groupe" => "default","models\\Organization" => "default","models\\Organizationsettings" => "default","models\\Settings" => "default","models\\User" => "default" ] );
+\Ubiquity\orm\DAO::setModelsDatabases ( $modelsDatabase );
 Router::startAll ();
 Router::addRoute ( "_default", "controllers\\IndexController" );
 \Ubiquity\assets\AssetsManager::start ( $config );
