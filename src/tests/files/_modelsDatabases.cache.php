c9872cb25c3342d284f29d001ebf4693f80c4148
jcheron <myaddressmail@gmail.com>
2020-08-29 14:53:08 +0200

    [ci-tests] add _modelsDatabases.cache

---
+++ src/tests/files/_modelsDatabases.cache.php
@@ -0,0 +1,2 @@
+<?php
+return [ "models\\bench\\Fortune" => "bench","models\\bench\\World" => "bench","models\\Groupe" => "default","models\\Organization" => "default","models\\Organizationsettings" => "default","models\\Settings" => "default","models\\User" => "default" ];
