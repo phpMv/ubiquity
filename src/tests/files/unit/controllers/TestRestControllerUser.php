9fd1fda25ad4261c8c5921d64a9a0c77a68066ad
jcheron <myaddressmail@gmail.com>
2020-09-03 01:52:38 +0200

    [ci-tests] start RestController test

---
+++ src/tests/files/unit/controllers/TestRestControllerUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace controllers;
+
+use Ubiquity\controllers\rest\RestController;
+
+/**
+ *
+ * @route("/rest/simple/user","inherited"=>true,"automated"=>true)
+ * @rest("resource"=>"User")
+ */
+class TestRestControllerUser extends RestController {
+}
+
