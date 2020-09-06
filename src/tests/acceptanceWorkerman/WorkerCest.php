0f6431603af9ed79c80fc4e8a64ec6a6f2952b63
jcheron <myaddressmail@gmail.com>
2020-09-07 01:56:22 +0200

    Update WorkerCest.php

--- src/tests/acceptanceWorkerman/WorkerCest.php
+++ src/tests/acceptanceWorkerman/WorkerCest.php
@@ -9,7 +9,7 @@ class WorkerCest {
 		$I->amOnPage ( "/fortunes" );
 		$I->seeElement ( 'body' );
 		$I->see ( 'fortune: No such file or directory', [ 'css' => 'body' ] );
-		$I->see ( "A computer scientist is someone who fixes things that aren''t broken.", [ 'css' => 'body' ] );
+		$I->see ( "A computer scientist is someone who fixes things that aren't broken.", [ 'css' => 'body' ] );
 		$I->see ( '<script>alert("This should not be displayed in a browser alert box.");</script>', [ 'css' => 'body' ] );
 		$I->see ( 'Feature: A bug with seniority.', [ 'css' => 'body' ] );
 	}
