fce7288e9540f4efc5993add86aeb56a694ae21b
jcheron <myaddressmail@gmail.com>
2020-09-07 02:13:50 +0200

    [ci-tests] add c3.php

--- src/tests/files/workerman/_workerman.php
+++ src/tests/files/workerman/_workerman.php
@@ -1,9 +1,11 @@
 #!/usr/bin/env php
 <?php
 // workerman.php
+include '../c3.php';
 if (! defined ( 'DS' )) {
 	define ( 'DS', DIRECTORY_SEPARATOR );
 	define ( 'ROOT', __DIR__ . \DS . '..' . \DS . 'app' . \DS );
+	define ( 'MY_APP_STARTED', true );
 }
 $config = include ROOT . 'config/config.php';
 $sConfig = [ 'host' => '127.0.0.1','port' => 8095,'sessionName' => 'workerman' ];
@@ -16,4 +18,7 @@ $workerServer->init ( $config, __DIR__ );
 $workerServer->setDefaultCount ();
 $workerServer->daemonize ();
 require ROOT . 'config/workerServices.php';
+\Ubiquity\controllers\Router::get ( "c3/(.*?)", function () {
+	require ROOT . './../c3.php';
+} );
 $workerServer->run ( $sConfig ['host'], $sConfig ['port'], $sConfig ['socket'] ?? [ ]);
