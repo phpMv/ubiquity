c4f4d3ddffd7ebd4b958d6951cd0d3ac84096f77
jcheron <myaddressmail@gmail.com>
2020-09-07 15:29:16 +0200

    [ci-tests] fix c3 url pb

--- src/tests/files/workerman/_workerman.php
+++ src/tests/files/workerman/_workerman.php
@@ -1,7 +1,6 @@
 #!/usr/bin/env php
 <?php
 // workerman.php
-include '../c3.php';
 if (! defined ( 'DS' )) {
 	define ( 'DS', DIRECTORY_SEPARATOR );
 	define ( 'ROOT', __DIR__ . \DS . '..' . \DS . 'app' . \DS );
@@ -12,13 +11,21 @@ $sConfig = [ 'host' => '127.0.0.1','port' => 8095,'sessionName' => 'workerman' ]
 $config ["sessionName"] = $sConfig ["sessionName"];
 $address = $sConfig ['host'] . ':' . $sConfig ['port'];
 $config ["siteUrl"] = 'http://' . $address;
-require ROOT . './../vendor/autoload.php';
-$workerServer = new \Ubiquity\servers\workerman\WorkermanServer ();
+require \ROOT . './../vendor/autoload.php';
+class MyWorker extends \Ubiquity\servers\workerman\WorkermanServer {
+
+	protected function handle(\Workerman\Connection\ConnectionInterface $connection, $datas) {
+		include \ROOT . '/../c3.php';
+		return parent::handle ( $connection, $datas );
+	}
+}
+
+$workerServer = new \MyWorker ();
 $workerServer->init ( $config, __DIR__ );
 $workerServer->setDefaultCount ();
 $workerServer->daemonize ();
 require ROOT . 'config/workerServices.php';
 \Ubiquity\controllers\Router::get ( "c3/(.*?)", function () {
-	require ROOT . './../c3.php';
+	require \ROOT . './../c3.php';
 } );
 $workerServer->run ( $sConfig ['host'], $sConfig ['port'], $sConfig ['socket'] ?? [ ]);
