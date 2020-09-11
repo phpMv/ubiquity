363243375c6d081145f7b3de6b23001c81fa6940
jcheron <myaddressmail@gmail.com>
2020-09-11 17:36:20 +0200

    [ci-tests] Add DAOTest with ObjectCache

---
+++ src/tests/unit/dao/DAOObjectTest.php
@@ -0,0 +1,35 @@
+<?php
+use Ubiquity\orm\DAO;
+use models\User;
+use models\Organization;
+use Ubiquity\db\Database;
+use models\Groupe;
+use Ubiquity\controllers\Startup;
+use Ubiquity\cache\CacheManager;
+
+/**
+ * DAO test case.
+ */
+class DAOObjectTest extends DAOTest {
+
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function _before() {
+		parent::_before();
+		CacheManager::start($this->config);
+		CacheManager::initCache($this->config,'models',true);
+		$this->dao = new DAO ();
+		$this->_startCache ();
+		$this->_startDatabase ( $this->dao );
+		$this->dao->prepareGetById ( "orga", Organization::class );
+		$this->dao->prepareGetOne ( "oneOrga", Organization::class, 'id= ?' );
+		$this->dao->prepareGetAll ( "orgas", Organization::class );
+	}
+
+	protected function getCacheDirectory() {
+		return 'cache-objects/';
+	}
+}
+
