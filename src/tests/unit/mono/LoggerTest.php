a09f822696ff5850a2ea7f99a202e178376e12c1
jcheron <myaddressmail@gmail.com>
2020-09-13 16:42:50 +0200

    [ci-tests] clearAll logs before

--- src/tests/unit/mono/LoggerTest.php
+++ src/tests/unit/mono/LoggerTest.php
@@ -20,6 +20,7 @@ class LoggerTest extends BaseTest {
 		$this->logger=new UMonolog('tests');
 		$this->config ["debug"] = true;
 		$this->logger->init ( $this->config );
+		$this->logger->clearAll ();
 		$this->_startServices ();
 		$this->_initRequest ( 'TestController', 'GET' );
 	}
@@ -33,7 +34,7 @@ class LoggerTest extends BaseTest {
 	 * Cleans up the environment after running a test.
 	 */
 	protected function _after() {
-		$this->logger->clearAll ();
+		$this->logger=null;
 		$this->config ["debug"] = false;
 	}
 
@@ -45,7 +46,7 @@ class LoggerTest extends BaseTest {
 		$this->assertEquals ( 0, sizeof ( $logs ) );
 		$this->_initRequest ( 'TestController', 'GET' );
 		Startup::run ( $this->config );
-		$logs = $this->logger->asObjects ( false, null, LoggerParams::ROUTER );
+		$logs = $this->logger->asObjects ( false, null, [LoggerParams::ROUTER] );
 		$this->assertEquals ( 1, sizeof ( $logs ) );
 		$this->assertInstanceOf ( LogMessage::class, $logs [0] );
 	}
@@ -58,7 +59,7 @@ class LoggerTest extends BaseTest {
 		$this->assertEquals ( 0, sizeof ( $logs ) );
 		$this->_initRequest ( '/TestCrudController', 'GET' );
 		Startup::run ( $this->config );
-		$logs = $this->logger->asObjects ( false, null, LoggerParams::DATABASE );
+		$logs = $this->logger->asObjects ( false, null, [LoggerParams::DATABASE] );
 		$this->assertEquals ( 6, sizeof ( $logs ) );
 		$this->assertInstanceOf ( LogMessage::class, $logs [0] );
 		$log = $logs [0];
@@ -74,7 +75,7 @@ class LoggerTest extends BaseTest {
 		$this->assertEquals ( 0, count ( $logs ) );
 		$this->_initRequest ( '/TestController/logs', 'GET' );
 		Startup::run ( $this->config );
-		$logs = $this->logger->asObjects ( false, null, 'logs' );
+		$logs = $this->logger->asObjects ( false, null, ['logs'] );
 		$this->assertEquals ( 3, sizeof ( $logs ) );
 		$log = $logs [0];
 		$this->assertEquals ( "critical", $log->getLevel () );
