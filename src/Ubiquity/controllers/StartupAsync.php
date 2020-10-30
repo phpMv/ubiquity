170ea6e62da1ac507e522f7fb94e0f8f9f854d2c
jcheron <myaddressmail@gmail.com>
2020-10-30 04:04:15 +0100

    Update StartupAsync.php

--- src/Ubiquity/controllers/StartupAsync.php
+++ src/Ubiquity/controllers/StartupAsync.php
@@ -19,10 +19,53 @@ class StartupAsync extends Startup {
 	private const FINALIZE = 4;
 	private static $controllers = [ ];
 
+	private static $routes=[];
+
+	/**
+	 * Forwards to url
+	 *
+	 * @param string $url The url to forward to
+	 * @param boolean $initialize If true, the **initialize** method of the controller is called
+	 * @param boolean $finalize If true, the **finalize** method of the controller is called
+	 */
+	public static function forward($url, $initialize = true, $finalize = true): void {
+		$methodUrl=$_SERVER['REQUEST_METHOD'].$url;
+		if($m=(self::$routes[$methodUrl]??false)){
+			$m($initialize,$finalize);
+			return ;
+		}
+		$u = self::parseUrl ( $url );
+		if (\is_array ( Router::getRoutes () ) && ($ru = Router::getRoute ( $url, true, self::$config ['debug'] ?? false)) !== false) {
+			if (\is_array ( $ru )) {
+				if (\is_string ( $ru [0] )) {
+					$xu=['ctrl'=>$ru[0],'action'=>$ru[1]??'index','params'=>\array_slice ( $ru, 2 )];
+					(self::$routes[$methodUrl]=function($i,$f)use($xu){
+						static::runAction ( $xu, $i, $f );
+					})($initialize,$finalize);
+				} else {
+					(self::$routes[$methodUrl]=function() use($ru){
+						self::runCallable ( $ru );
+					})();
+				}
+			} else {
+				(self::$routes[$methodUrl]=function() use($ru){
+					echo $ru; // Displays route response from cache
+				})();
+
+			}
+		} else {
+			$u [0] = self::$ctrlNS . $u [0];
+			$xu=['ctrl'=>$u[0],'action'=>$u[1]??'index','params'=>\array_slice ( $u, 2 )];
+			(self::$routes[$methodUrl]=function($i,$f)use($xu){
+				static::runAction ( $xu, $i, $f );
+			})($initialize,$finalize);
+		}
+	}
+
 	public static function runAction(array &$u, $initialize = true, $finalize = true): void {
-		self::$controller = $ctrl = $u [0];
-		self::$action = $action = $u [1] ?? 'index';
-		self::$actionParams = \array_slice ( $u, 2 );
+		self::$controller = $ctrl = $u ['ctrl'];
+		self::$action = $action = $u ['action'];
+		self::$actionParams = $u['params'];
 
 		try {
 			if (null !== $controller = self::getControllerInstance ( $ctrl )) {
