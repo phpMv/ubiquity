ff7a78ee1595aabb6223bd6d39867cf75c6c5703
jcheron <myaddressmail@gmail.com>
2020-12-07 02:29:20 +0100

    [skip ci] Fix default route parameter with getRouteByName
    with twig,
    no more need to use `path('routeName',[''])` for generating a route without param

--- src/Ubiquity/controllers/Router.php
+++ src/Ubiquity/controllers/Router.php
@@ -15,7 +15,7 @@ use Ubiquity\utils\http\URequest;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.13
+ * @version 1.0.14
  *
  */
 class Router {
@@ -46,7 +46,7 @@ class Router {
 		$result = \preg_replace_callback ( '~\((.*?)\)~', function () use (&$params) {
 			return \array_shift ( $params );
 		}, $routePath );
-		if (\sizeof ( $params ) > 0) {
+		if (\count ( $params ) > 0) {
 			$result = \rtrim ( $result, '/' ) . '/' . \implode ( '/', $params );
 		}
 		return $result;
@@ -132,17 +132,17 @@ class Router {
 	 * Returns the generated path from a route
 	 *
 	 * @param string $name name of the route
-	 * @param array $parameters array of the route parameters. default : []
+	 * @param array $parameters array of the route parameters. default : ['']
 	 * @param boolean $absolute
 	 */
-	public static function getRouteByName($name, $parameters = [ ], $absolute = true) {
+	public static function getRouteByName($name, $parameters = [''], $absolute = true) {
 		foreach ( self::$routes as $routePath => $routeDetails ) {
 			if (self::checkRouteName ( $routeDetails, $name )) {
 				if (\sizeof ( $parameters ) > 0) {
 					$routePath = self::_getURL ( $routePath, $parameters );
 				}
 				if (trim ( $routePath, '/' ) == '_default') {
-					$routePath = "/";
+					$routePath = '/';
 				}
 				if (! $absolute) {
 					return \ltrim ( $routePath, '/' );
@@ -167,11 +167,11 @@ class Router {
 	 * Returns the generated path from a route
 	 *
 	 * @param string $name The route name
-	 * @param array $parameters default: []
+	 * @param array $parameters default: ['']
 	 * @param boolean $absolute true if the path is absolute (/ at first)
 	 * @return boolean|string|array|mixed the generated path (/path/to/route)
 	 */
-	public static function path($name, $parameters = [ ], $absolute = false) {
+	public static function path($name, $parameters = [''], $absolute = false) {
 		return self::getRouteByName ( $name, $parameters, $absolute );
 	}
 
@@ -182,7 +182,7 @@ class Router {
 	 * @param array $parameters default: []
 	 * @return string the generated url (http://myApp/path/to/route)
 	 */
-	public static function url($name, $parameters = [ ]): string {
+	public static function url($name, $parameters = ['']): string {
 		return URequest::getUrl ( self::getRouteByName ( $name, $parameters, false ) );
 	}
 
