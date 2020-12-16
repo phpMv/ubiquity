63b9716fb04d444439838895097e78ec312a4fd8
jcheron <myaddressmail@gmail.com>
2020-12-17 00:04:36 +0100

    Update DevCacheTrait.php

--- src/Ubiquity/cache/traits/DevCacheTrait.php
+++ src/Ubiquity/cache/traits/DevCacheTrait.php
@@ -27,10 +27,11 @@ trait DevCacheTrait {
 	abstract public static function initModelsCache(&$config, $forChecking = false, $silent = false);
 
 	private static function _getAnnotationsEngineInstance(){
+		var_dump(\class_exists('Ubiquity\\attributes\\AttributesEngine'));
 		if(\class_exists('Ubiquity\\attributes\\AttributesEngine',true)){
-			$instance=new \Ubiquity\attributes\AttributesEngine();
+			return new \Ubiquity\attributes\AttributesEngine();
 		}elseif(\class_exists('Ubiquity\\annotations\\AnnotationsEngine',true)){
-			$instance=new \Ubiquity\annotations\AnnotationsEngine();
+			return new \Ubiquity\annotations\AnnotationsEngine();
 		}
 	}
 
