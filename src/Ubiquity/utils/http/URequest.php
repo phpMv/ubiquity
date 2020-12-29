27e48c0a5fe654cf4e28f3e0a3b4d10cf4b17661
jcheron <myaddressmail@gmail.com>
2020-12-29 20:38:15 +0100

    split URequest

--- src/Ubiquity/utils/http/URequest.php
+++ src/Ubiquity/utils/http/URequest.php
@@ -4,6 +4,7 @@ namespace Ubiquity\utils\http;
 
 use Ubiquity\controllers\Startup;
 use Ubiquity\utils\base\UString;
+use Ubiquity\utils\http\traits\URequestTesterTrait;
 
 /**
  * Http Request utilities, wrapper for accessing to $_GET, $_POST and php://input.
@@ -11,11 +12,11 @@ use Ubiquity\utils\base\UString;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.1.2
+ * @version 1.1.3
  *
  */
 class URequest {
-
+use URequestTesterTrait;
 	/**
 	 * Affects member to member the values of the associative array $values to the members of the object $object
 	 * Used for example to retrieve the variables posted and assign them to the members of an object
@@ -94,27 +95,6 @@ class URequest {
 		return self::getInput ();
 	}
 
-	/**
-	 * Tests if a value is present on the request and is not empty
-	 *
-	 * @param string $key
-	 *
-	 * @return bool
-	 */
-	public static function filled($key): bool {
-		return isset ( $_REQUEST [$key] ) && $_REQUEST [$key] != null;
-	}
-
-	/**
-	 * Tests if a value is present on the request
-	 *
-	 * @param string $key
-	 *
-	 * @return bool
-	 */
-	public static function has($key): bool {
-		return isset ( $_REQUEST [$key] );
-	}
 
 	/**
 	 * Adds a value in request at $key position
@@ -125,19 +105,6 @@ class URequest {
 	 */
 	public static function set(string $key, $value = true) {
 		return $_REQUEST [$key] = $value;
-	}
-
-	/**
-	 * Returns the request content-type header
-	 *
-	 * @return string
-	 */
-	public static function getContentType(): ?string {
-		$headers = Startup::getHttpInstance ()->getAllHeaders ();
-		if (isset ( $headers ['Content-Type'] )) {
-			return $headers ['Content-Type'];
-		}
-		return null;
 	}
 
 	/**
@@ -185,42 +152,6 @@ class URequest {
 		}
 	}
 
-	/**
-	 * Returns true if the request is an Ajax request
-	 *
-	 * @return boolean
-	 */
-	public static function isAjax(): bool {
-		return (isset ( $_SERVER ['HTTP_X_REQUESTED_WITH'] ) && ! empty ( $_SERVER ['HTTP_X_REQUESTED_WITH'] ) && strtolower ( $_SERVER ['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest');
-	}
-
-	/**
-	 * Returns true if the request is sent by the POST method
-	 *
-	 * @return boolean
-	 */
-	public static function isPost(): bool {
-		return $_SERVER ['REQUEST_METHOD'] === 'POST';
-	}
-
-	/**
-	 * Returns true if the request is cross site
-	 *
-	 * @return boolean
-	 */
-	public static function isCrossSite(): bool {
-		return \stripos ( $_SERVER ['HTTP_REFERER'], $_SERVER ['SERVER_NAME'] ) === FALSE;
-	}
-
-	/**
-	 * Returns true if request contentType is set to json
-	 *
-	 * @return boolean
-	 */
-	public static function isJSON(): bool {
-		$contentType = self::getContentType ();
-		return \stripos ( $contentType, 'json' ) !== false;
-	}
 
 	/**
 	 * Returns the value of the $key variable passed by the get method or $default if the $key variable does not exist
