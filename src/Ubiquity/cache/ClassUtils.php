3e22d66fcadf33b688085a5dc5679dea1e82af23
jcheron <myaddressmail@gmail.com>
2021-04-21 01:31:45 +0200

    [skip ci][Debug] update getClassNameFromPhpCode for Traits

--- src/Ubiquity/cache/ClassUtils.php
+++ src/Ubiquity/cache/ClassUtils.php
@@ -49,7 +49,7 @@ class ClassUtils {
 		$tokens = \token_get_all ( $phpCode );
 		$count = count ( $tokens );
 		for($i = 2; $i < $count; $i ++) {
-			if ($tokens [$i - 2] [0] == T_CLASS && $tokens [$i - 1] [0] == T_WHITESPACE && $tokens [$i] [0] == T_STRING) {
+			if (($tokens [$i - 2] [0] == T_TRAIT || $tokens [$i - 2] [0] == T_CLASS )&& $tokens [$i - 1] [0] == T_WHITESPACE && $tokens [$i] [0] == T_STRING) {
 				$class_name = $tokens [$i] [1];
 				$classes [] = $class_name;
 			}
