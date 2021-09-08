c536680fd2aded7e6e2b11f4400b4318f93fdaf0
J-C HERON <myaddressmail@gmail.com>
2021-09-08 09:24:31 +0200

    Update ApacheWorkerCest.php

--- src/tests/acceptance/ApacheWorkerCest.php
+++ src/tests/acceptance/ApacheWorkerCest.php
@@ -8,7 +8,7 @@ class ApacheWorkerCest {
 	public function tryToGotoFortunes(AcceptanceTester $I) {
 		$I->amOnPage ( "/fortunes" );
 		$I->seeElement ( 'body' );
-		$I->see ( 'fortune: No such file or directory', [ 'css' => 'body' ] );
+		$I->see ( 'Fortune: No such file or directory', [ 'css' => 'body' ] );
 		$I->see ( "A computer scientist is someone who fixes things that aren't broken.", [ 'css' => 'body' ] );
 		$I->see ( '<script>alert("This should not be displayed in a browser alert box.");</script>', [ 'css' => 'body' ] );
 		$I->see ( 'Feature: A bug with seniority.', [ 'css' => 'body' ] );
