d3a6dc745312ba8fbf9b1c3180880f233460a0e1
jcheron <myaddressmail@gmail.com>
2021-10-20 02:14:59 +0200

    [skip ci] add getBase + createDomain

--- src/Ubiquity/domains/DDDManager.php
+++ src/Ubiquity/domains/DDDManager.php
@@ -4,6 +4,7 @@
 namespace Ubiquity\domains;
 
 use Ubiquity\controllers\Startup;
+use Ubiquity\utils\base\UFileSystem;
 
 /**
  * Manager for a Domain Driven Design approach.
@@ -39,11 +40,11 @@ class DDDManager {
 	public static function hasDomains(): bool {
 		return \file_exists(\ROOT.self::$base) && \count(self::getDomains())>0;
 	}
-
+	
 	public static function getActiveDomain(): string {
 		return self::$activeDomain;
 	}
-
+	
 	public static function getActiveViewFolder(){
 		if(self::$activeDomain!=''){
 			if(\file_exists($folder=\ROOT.self::$base.\DS.self::$activeDomain.\DS.'views'.\DS)){
@@ -52,16 +53,43 @@ class DDDManager {
 		}
 		return \ROOT.'views'.\DS;
 	}
-
+	
 	public static function getViewNamespace(){
 		$activeDomain=self::$activeDomain;
 		if($activeDomain!=''){
 			return '@'.$activeDomain.'/';
 		}
-		return '';
+		return '';		
 	}
-
+	
 	public static function getDomainBase($domain){
 		return self::$base.\DS. \trim($domain, '\\') . '\\';
 	}
+
+	public static function createDomain($domainName){
+		$baseFolder=$folder=\ROOT.self::$base.\DS.$domainName.\DS;
+		$result=self::createFolder($baseFolder.'views');
+		if($result) {
+			$result = self::createFolder($baseFolder . (Startup::$config['mvcNS']['controllers']) ?? 'controllers');
+			if($result){
+				$result=self::createFolder($baseFolder . (Startup::$config['mvcNS']['models']) ?? 'models');
+			}
+		}
+		return $result;
+	}
+
+	private static function createFolder(string $folder): bool {
+		if(UFileSystem::safeMkdir($folder)){
+			return false!==\file_put_contents($folder.\DS.'.gitkeep','');
+		}
+		return false;
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function getBase(): string {
+		return self::$base;
+	}
+	
 }
