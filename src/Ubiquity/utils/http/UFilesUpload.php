826a96fcd6ef6724f3dee56161920141f60042ee
jcheron <myaddressmail@gmail.com>
2021-11-08 19:52:35 +0100

    [skip ci] add messageTypes for UFileUpload

--- src/Ubiquity/utils/http/UFilesUpload.php
+++ src/Ubiquity/utils/http/UFilesUpload.php
@@ -17,17 +17,34 @@ use Ubiquity\utils\base\UFileSystem;
  */
 
 class UFilesUpload {
+	public const SUCCESS_MESSAGE='success';
+	public const NO_FILE_SENT_ERR='noFileSentErr';
+	public const FILE_SIZE_ERR='fileSizeErr';
+	public const UNKNOWN_ERR='unknownErr';
+	public const MIME_TYPE_ERR='mimeTypeErr';
+	public const EXISTING_FILE_ERR='existingFileErr';
+	public const UPLOAD_ERR='uploadErr';
+	
 	private const IMAGES_MIME_TYPES=['bmp'=>'image/bmp','gif'=>'image/gif','ico'=>'image/vnd.microsoft.icon','jpg'=>'image/jpeg','jpeg'=>'image/jpeg','svg'=>'image/svg+xml','png'=>'image/png','tif'=>'image/tiff','tiff'=>'image/tiff'];
 	
 	private int $maxFileSize=100000;
 	private ?array $allowedMimeTypes=['pdf'=>'application/pdf'];
 	private array $messages;
 	private string $destDir;
-
+	private array $messageTypes=[
+			self::SUCCESS_MESSAGE=>'The file %s has been uploaded.',
+			self::NO_FILE_SENT_ERR=>'No file sent.',
+			self::FILE_SIZE_ERR=>'Exceeded file size limit for %s.',
+			self::UNKNOWN_ERR=>'Unknown Error.',
+			self::MIME_TYPE_ERR=>'The mime-type %s is not allowed for %s!',
+			self::EXISTING_FILE_ERR=>'Sorry, The file %s already exists.',
+			self::UPLOAD_ERR=>'Sorry, there was an error uploading the file %s.'
+	];
+	
 	public function __construct($destDir='upload') {
 		$this->destDir=$destDir;
 	}
-
+	
 	/**
 	 * @param array|null $allowedMimeTypes
 	 * @param int $maxFileSize
@@ -36,28 +53,28 @@ class UFilesUpload {
 		$this->allowedMimeTypes=$allowedMimeTypes;
 		$this->maxFileSize=$maxFileSize;
 	}
-
+	
 	/**
 	 * @return int
 	 */
 	public function getMaxFileSize(): int {
 		return $this->maxFileSize;
 	}
-
+	
 	/**
 	 * @param int $maxFileSize
 	 */
 	public function setMaxFileSize(int $maxFileSize): void {
 		$this->maxFileSize = $maxFileSize;
 	}
-
+	
 	/**
 	 * @return array|string[]|null
 	 */
 	public function getAllowedMimeTypes(): ?array {
 		return $this->allowedMimeTypes;
 	}
-
+	
 	/**
 	 * @param array|string[]|null $allowedMimeTypes
 	 */
@@ -66,58 +83,82 @@ class UFilesUpload {
 	}
 	
 	public function allowImages(bool $only=true): void {
-		if($only){
-			$this->allowedMimeTypes=self::IMAGES_MIME_TYPES;
-		}else{
-			$this->allowedMimeTypes+=self::IMAGES_MIME_TYPES;
-		}
+		$this->allowType($only,self::IMAGES_MIME_TYPES);
 	}
-
+	
 	public function allowAllMimeTypes(): void{
 		$this->allowedMimeTypes=null;
 	}
 	
+	private function allowType(bool $only,array $typeArray): void {
+		if($only){
+			$this->allowedMimeTypes=$typeArray;
+		}else{
+			$this->allowedMimeTypes+=$typeArray;
+		}
+	}
+	
 	private function checkErrors(array $file):void{
-		if (!isset($file['error']) ||	\is_array($file['error'])){
+		if (!isset($file['error']) || \is_array($file['error'])){
 			throw new FileUploadException('Invalid error format.');
 		}
 		switch ($file['error']) {
 			case \UPLOAD_ERR_OK:
 				break;
 			case \UPLOAD_ERR_NO_FILE:
-				throw new FileUploadException('No file sent.');
+				throw new FileUploadException($this->getMessageType(self::NO_FILE_SENT_ERR));
 			case \UPLOAD_ERR_INI_SIZE:
 			case \UPLOAD_ERR_FORM_SIZE:
-				throw new FileUploadException('Exceeded file size limit.');
+				throw new FileUploadException($this->getMessageType(self::FILE_SIZE_ERR,$this->getDisplayedFileName($file)));
 			default:
-				throw new FileUploadException('Unknown errors.');
+				throw new FileUploadException($this->getMessageType(self::UNKNOWN_ERR,$this->getDisplayedFileName($file)));
 		}
 	}
-
+	
+	private function getMessageType(string $type,string ...$params){
+		return \vsprintf($this->messageTypes[$type]??'No message', $params);
+	}
+	
 	private function checkTypeMime(array $file): bool {
 		$allowedMimeTypes=$this->getAllowedMimeTypes();
 		if(\is_array($allowedMimeTypes)) {
 			$finfo = new \finfo(\FILEINFO_MIME_TYPE);
 			if(\array_search($finfo->file($file['tmp_name']), $this->getAllowedMimeTypes(), true) === false){
-				$this->messages['errors'][]=\sprintf('The mime-type %s is not allowed for %s!',$finfo->file($file['tmp_name']),$this->getDisplayedFileName($file));
+				$this->messages['errors'][]=$this->getMessageType(self::MIME_TYPE_ERR, $finfo->file($file['tmp_name']),$this->getDisplayedFileName($file));
 				return false;
 			}
 		}
 		return true;
 	}
-
+	
 	private function checkFileSize(array $file): bool{
 		if ($file['size'] > $this->maxFileSize) {
-			$this->messages['errors'][]=\sprintf('Exceeded file size limit for %s.',$this->getDisplayedFileName($file));
+			$this->messages['errors'][]=$this->getMessageType(self::FILE_SIZE_ERR,$this->getDisplayedFileName($file));
 			return false;
 		}
 		return true;
 	}
-
+	
 	private function getDisplayedFileName(array $file): string {
-		return \htmlspecialchars(basename($file['name']));
+		return \htmlspecialchars(\basename($file['name']));
 	}
-
+	
+	/**
+	 * Redefine the messages displayed.
+	 * @param array $messages
+	 */
+	public function setMessageTypes(array $messages): void {
+		$this->messageTypes=\array_merge($this->messageTypes,$messages);
+	}
+	
+	/**
+	 * Get the message types displayed.
+	 * @return array
+	 */
+	public function getMessageTypes(): array {
+		return $this->messageTypes;
+	}
+	
 	/**
 	 * Uploads files to $destDir directory.
 	 * @param string $destDir is relative to ROOT app
@@ -133,14 +174,15 @@ class UFilesUpload {
 				$this->checkErrors($file);
 				if($this->checkTypeMime($file) && $this->checkFileSize($file)) {
 					$filename = \basename($file['name']);
+					$dFileName=\htmlspecialchars($filename);
 					if ($force || !\file_exists($dest . $filename)) {
 						if (\move_uploaded_file($file['tmp_name'], $dest . $filename)) {
-							$this->messages['success'][] = 'The file ' . \htmlspecialchars($filename) . ' has been uploaded.';
+							$this->messages['success'][] = $this->getMessageType(self::SUCCESS_MESSAGE, $dFileName);
 						} else {
-							$this->messages['errors'][] = 'Sorry, there was an error uploading the file.' . \htmlspecialchars($filename);
+							$this->messages['errors'][] = $this->getMessageType(self::UPLOAD_ERR, $dFileName);
 						}
 					} else {
-						$this->messages['errors'][] = 'Sorry, The file ' . \htmlspecialchars($filename) . ' already exists.';
+						$this->messages['errors'][] = $this->getMessageType(self::EXISTING_FILE_ERR, $dFileName);
 					}
 				}
 			}
@@ -148,7 +190,7 @@ class UFilesUpload {
 			$this->messages['errors'][]=$e->getMessage();
 		}
 	}
-
+	
 	/**
 	 * Returns true true if the upload generated at least one error.
 	 * @return bool
@@ -156,7 +198,7 @@ class UFilesUpload {
 	public function hasErrors(): bool {
 		return isset($this->messages['errors']) && \count($this->messages['errors'])>0;
 	}
-
+	
 	/**
 	 * Returns true if at least one file has been uploaded.
 	 * @return bool
@@ -164,7 +206,7 @@ class UFilesUpload {
 	public function isSuccess(): bool {
 		return isset($this->messages['success']) && \count($this->messages['success'])>0;
 	}
-
+	
 	/**
 	 * Returns all success messages.
 	 * @return array|mixed
@@ -172,7 +214,7 @@ class UFilesUpload {
 	public function getMessages(): array {
 		return $this->messages['success']??[];
 	}
-
+	
 	/**
 	 * Returns all error messages.
 	 * @return array|mixed
