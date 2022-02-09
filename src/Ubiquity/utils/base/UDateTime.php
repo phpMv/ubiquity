4fa89984a76f244c88df537583881990cef32500
jcheron <myaddressmail@gmail.com>
2022-02-09 12:58:51 +0100

    [tests] fix scrut failure

--- src/Ubiquity/utils/base/UDateTime.php
+++ src/Ubiquity/utils/base/UDateTime.php
@@ -14,18 +14,18 @@ namespace Ubiquity\utils\base;
 class UDateTime {
 	const MYSQL_DATETIME_FORMAT = 'Y-m-d H:i:s';
 	const MYSQL_DATE_FORMAT = 'Y-m-d';
-	private static $locales = [ "fr" => [ "fr","fr_FR","fr_FR.UTF-8" ],"en" => [ "en","en_US","en_US.UTF-8" ] ];
+	private static $locales = [ 'fr' => [ 'fr','fr_FR','fr_FR.UTF-8' ],'en' => [ 'en','en_US','en_US.UTF-8' ] ];
 
 	private static function setLocale($locale) {
 		$locale = self::$locales [$locale]??self::$locales ["en"];
-		setlocale ( LC_TIME, $locale [0], $locale [1], $locale [2] );
+		\setlocale ( LC_TIME, $locale [0], $locale [1], $locale [2] );
 	}
 
 	public static function secondsToTime($seconds) {
-		$hours = floor ( $seconds / 3600 );
-		$mins = floor ( $seconds / 60 % 60 );
-		$secs = floor ( $seconds % 60 );
-		return sprintf ( '%02d:%02d:%02d', $hours, $mins, $secs );
+		$hours = \floor ( $seconds / 3600 );
+		$mins = \floor ( $seconds / 60 % 60 );
+		$secs = \floor ( $seconds % 60 );
+		return \sprintf ( '%02d:%02d:%02d', $hours, $mins, $secs );
 	}
 
 	public static function mysqlDate(\DateTime $date) {
@@ -36,24 +36,24 @@ class UDateTime {
 		return $datetime->format ( self::MYSQL_DATETIME_FORMAT );
 	}
 
-	public static function longDate($date, $locale = "en") {
+	public static function longDate($date, $locale = 'en') {
 		self::setLocale ( $locale );
 		return \date('l d F Y',\strtotime($date));
 	}
 
-	public static function shortDate($date, $locale = "en") {
+	public static function shortDate($date, $locale = 'en') {
 		self::setLocale ( $locale );
 		return \date('d/m/y',\strtotime($date));
 	}
 
-	public static function shortDatetime($datetime, $locale = "en") {
+	public static function shortDatetime($datetime, $locale = 'en') {
 		self::setLocale ( $locale );
-		return \date('c',\strtotime($date));
+		return \date('c',\strtotime($datetime));
 	}
 
-	public static function longDatetime($datetime, $locale = "en") {
+	public static function longDatetime($datetime, $locale = 'en') {
 		self::setLocale ( $locale );
-		return date('l d F Y, H:i:s', \strtotime ( $datetime ));
+		return \date('l d F Y, H:i:s', \strtotime ( $datetime ));
 	}
 
 	/**
@@ -72,10 +72,10 @@ class UDateTime {
 		}
 		$diff = $now->diff ( $ago );
 
-		$diff->w = floor ( $diff->d / 7 );
+		$diff->w = \floor ( $diff->d / 7 );
 		$diff->d -= $diff->w * 7;
 
-		$string = array ('y' => 'year','m' => 'month','w' => 'week','d' => 'day','h' => 'hour','i' => 'minute','s' => 'second' );
+		$string = ['y' => 'year','m' => 'month','w' => 'week','d' => 'day','h' => 'hour','i' => 'minute','s' => 'second' ];
 		foreach ( $string as $k => &$v ) {
 			if ($diff->$k) {
 				$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
@@ -84,9 +84,10 @@ class UDateTime {
 			}
 		}
 
-		if (! $full)
+		if (! $full){
 			$string = \array_slice ( $string, 0, 1 );
-		return $string ? implode ( ', ', $string ) . ($diff->invert ? ' ago' : '') : 'just now';
+		}
+		return $string ? \implode ( ', ', $string ) . ($diff->invert ? ' ago' : '') : 'just now';
 	}
 }
 
