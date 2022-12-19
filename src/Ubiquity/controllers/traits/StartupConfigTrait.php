dbc71a300763a0ea80ed6e3bacbf89a2d335cf88
jcheron <myaddressmail@gmail.com>
2022-12-19 23:31:58 +0100

    [skip ci] fix reloadConfig bug with siteUrl

--- src/Ubiquity/controllers/traits/StartupConfigTrait.php
+++ src/Ubiquity/controllers/traits/StartupConfigTrait.php
@@ -19,7 +19,7 @@ use Ubiquity\utils\http\session\PhpSession;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.1.7
+ * @version 1.1.8
  *
  */
 trait StartupConfigTrait {
@@ -27,8 +27,9 @@ trait StartupConfigTrait {
 	protected static $ctrlNS;
 	protected static $httpInstance;
 	protected static $sessionInstance;
-	protected static $activeDomainBase='';
-	protected static $CONFIG_LOCATION='/config/';
+	protected static $activeDomainBase = '';
+	protected static $CONFIG_LOCATION = '/config/';
+	protected static $CONFIG_CACHE_LOCATION = 'cache/config/config.cache.php';
 
 	public static function getConfig(): array {
 		return self::$config;
@@ -39,57 +40,60 @@ trait StartupConfigTrait {
 	}
 
 	public static function getModelsDir(): string {
-		return \str_replace('\\',\DS,self::getNS('models'));
+		return \str_replace('\\', \DS, self::getNS('models'));
 	}
 
 	public static function getModelsCompletePath(): string {
-		return \ROOT . \DS . self::getModelsDir ();
+		return \ROOT . \DS . self::getModelsDir();
 	}
 
 	protected static function needsKeyInConfigArray(&$result, $array, $needs): void {
-		foreach ( $needs as $need ) {
-			if (! isset ( $array [$need] ) || UString::isNull ( $array [$need] )) {
+		foreach ($needs as $need) {
+			if (!isset ($array [$need]) || UString::isNull($array [$need])) {
 				$result [] = $need;
 			}
 		}
 	}
 
 	public static function getNS($part = 'controllers'): string {
-		return self::$activeDomainBase.((self::$config ['mvcNS'] [$part])??$part)."\\";
+		return self::$activeDomainBase . ((self::$config ['mvcNS'] [$part]) ?? $part) . "\\";
 	}
 
 	protected static function setCtrlNS(): string {
-		return self::$ctrlNS = self::getNS ();
+		return self::$ctrlNS = self::getNS();
 	}
 
 	public static function checkDbConfig($offset = 'default'): array {
 		$config = self::$config;
-		$result = [ ];
-		$needs = [ 'type','dbName','serverName' ];
-		if (! isset ( $config ['database'] )) {
+		$result = [];
+		$needs = ['type', 'dbName', 'serverName'];
+		if (!isset ($config ['database'])) {
 			$result [] = 'database';
 		} else {
-			self::needsKeyInConfigArray ( $result, DAO::getDbOffset ( $config, $offset ), $needs );
+			self::needsKeyInConfigArray($result, DAO::getDbOffset($config, $offset), $needs);
 		}
 		return $result;
 	}
 
 	public static function checkModelsConfig(): array {
 		$config = self::$config;
-		$result = [ ];
-		if (! isset ( $config ['mvcNS'] )) {
+		$result = [];
+		if (!isset ($config ['mvcNS'])) {
 			$result [] = 'mvcNS';
 		} else {
-			self::needsKeyInConfigArray ( $result, $config ['mvcNS'], [ 'models' ] );
+			self::needsKeyInConfigArray($result, $config ['mvcNS'], ['models']);
 		}
 		return $result;
 	}
 
-	public static function reloadConfig(): array {
-		$appDir = \dirname ( \ROOT );
-		$filename = $appDir . '/app'.static::$CONFIG_LOCATION.'config.php';
-		self::$config = include ($filename);
-		self::startTemplateEngine ( self::$config );
+	public static function reloadConfig(bool $preserveSiteUrl = true): array {
+		$siteUrl = self::$config['siteUrl'];
+		$filename = \ROOT . self::$CONFIG_CACHE_LOCATION;
+		self::$config = include($filename);
+		if ($preserveSiteUrl) {
+			self::$config ['siteUrl'] = $siteUrl;
+		}
+		self::startTemplateEngine(self::$config);
 		return self::$config;
 	}
 
@@ -98,30 +102,30 @@ trait StartupConfigTrait {
 		include \ROOT . 'config/services.php';
 	}
 
-	public static function saveConfig(array $contentArray,string $configFilename='config') {
-		$appDir = \dirname ( \ROOT );
+	public static function saveConfig(array $contentArray, string $configFilename = 'config') {
+		$appDir = \dirname(\ROOT);
 		$filename = $appDir . "/app/config/$configFilename.php";
 		$oldFilename = $appDir . '/app/config/config.old.php';
-		$content = "<?php\nreturn " . UArray::asPhpArray ( $contentArray, 'array', 1, true ) . ";";
-		if (CodeUtils::isValidCode ( $content )) {
-			if (! \file_exists ( $filename ) || \copy ( $filename, $oldFilename )) {
-				return UFileSystem::save ( $filename, $content );
+		$content = "<?php\nreturn " . UArray::asPhpArray($contentArray, 'array', 1, true) . ";";
+		if (CodeUtils::isValidCode($content)) {
+			if (!\file_exists($filename) || \copy($filename, $oldFilename)) {
+				return UFileSystem::save($filename, $content);
 			}
 		} else {
-			throw new InvalidCodeException ( 'Config contains invalid code' );
+			throw new InvalidCodeException ('Config contains invalid code');
 		}
 		return false;
 	}
 
-	public static function updateConfig(array $content,string $configFilename='config') {
-		foreach ( $content as $k => $v ) {
+	public static function updateConfig(array $content, string $configFilename = 'config') {
+		foreach ($content as $k => $v) {
 			self::$config [$k] = $v;
 		}
-		return self::saveConfig ( self::$config ,$configFilename);
+		return self::saveConfig(self::$config, $configFilename);
 	}
 
 	public static function getHttpInstance(): AbstractHttp {
-		if (! isset ( self::$httpInstance )) {
+		if (!isset (self::$httpInstance)) {
 			self::$httpInstance = new PhpHttp ();
 		}
 		return self::$httpInstance;
@@ -132,7 +136,7 @@ trait StartupConfigTrait {
 	}
 
 	public static function getSessionInstance(): AbstractSession {
-		if (! isset ( self::$sessionInstance )) {
+		if (!isset (self::$sessionInstance)) {
 			self::$sessionInstance = new PhpSession ();
 		}
 		return self::$sessionInstance;
@@ -143,25 +147,25 @@ trait StartupConfigTrait {
 	}
 
 	public static function isValidUrl(string $url): bool {
-		$u = self::parseUrl ( $url );
-		if (\is_array ( Router::getRoutes () ) && ($ru = Router::getRoute ( $url, false, self::$config ['debug'] ?? false)) !== false) {
-			if (\is_array ( $ru )) {
-				if (\is_string ( $ru ['controller'] )) {
-					return static::isValidControllerAction ( $ru ['controller'], $ru ['action'] ?? 'index');
+		$u = self::parseUrl($url);
+		if (\is_array(Router::getRoutes()) && ($ru = Router::getRoute($url, false, self::$config ['debug'] ?? false)) !== false) {
+			if (\is_array($ru)) {
+				if (\is_string($ru ['controller'])) {
+					return static::isValidControllerAction($ru ['controller'], $ru ['action'] ?? 'index');
 				} else {
-					return \is_callable ( $ru );
+					return \is_callable($ru);
 				}
 			}
 		} else {
 			$u [0] = self::$ctrlNS . $u [0];
-			return static::isValidControllerAction ( $u [0], $u [1] ?? 'index');
+			return static::isValidControllerAction($u [0], $u [1] ?? 'index');
 		}
 		return false;
 	}
 
 	private static function isValidControllerAction(string $controller, string $action): bool {
-		if (\class_exists ( $controller )) {
-			return \method_exists ( $controller, $action );
+		if (\class_exists($controller)) {
+			return \method_exists($controller, $action);
 		}
 		return false;
 	}
@@ -171,12 +175,12 @@ trait StartupConfigTrait {
 	 * @param string $domain The new active domain name
 	 * @param string $base The base folder for domains
 	 */
-	public static function setActiveDomainBase(string $domain,string $base='domains'): void {
-			self::$activeDomainBase = $base . '\\' . \trim($domain, '\\') . '\\';
-			if(isset(self::$templateEngine)){
-				$viewFolder=\realpath( \str_replace('\\',\DS,\ROOT.self::$activeDomainBase.'views'));
-				self::$templateEngine->setPaths([$viewFolder],$domain);
-			}
+	public static function setActiveDomainBase(string $domain, string $base = 'domains'): void {
+		self::$activeDomainBase = $base . '\\' . \trim($domain, '\\') . '\\';
+		if (isset(self::$templateEngine)) {
+			$viewFolder = \realpath(\str_replace('\\', \DS, \ROOT . self::$activeDomainBase . 'views'));
+			self::$templateEngine->setPaths([$viewFolder], $domain);
+		}
 	}
 
 	public static function getActiveDomainBase(): string {
@@ -184,6 +188,7 @@ trait StartupConfigTrait {
 	}
 
 	public static function resetActiveDomainBase(): void {
-		self::$activeDomainBase='';
+		self::$activeDomainBase = '';
 	}
 }
+
