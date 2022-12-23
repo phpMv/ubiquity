4064ce186dd8c649605473ac155d6fd359e97b04
jcheron <myaddressmail@gmail.com>
2022-12-23 11:57:42 +0100

    [doc] [skip ci] update language conf

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ author = 'phpmv'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "fr"
+language = "en"
 gettext_uuid = True
 gettext_compact = False
 locale_dirs = ['locales']
