6489a6e0e173875826c55b8998d2e6449fe95063
jcheron <myaddressmail@gmail.com>
2023-01-14 04:22:08 +0100

    [skip ci] fix nonce pb

--- src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
+++ src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
@@ -3,86 +3,90 @@
 namespace Ubiquity\views\engine\latte;
 
 use Ubiquity\utils\base\UArray;
+use Ubiquity\utils\base\UString;
 
 class LatteTemplateGenerator extends \Ubiquity\views\engine\TemplateGenerator {
-	
+
 	public function __construct() {
-	$this->openExpressionTag=$this->openVarTag='{';
-	$this->closeExpressionTag=$this->closeVarTag='}';
-	$this->safeFilter='noescape';
+		$this->openExpressionTag = $this->openVarTag = '{';
+		$this->closeExpressionTag = $this->closeVarTag = '}';
+		$this->safeFilter = 'noescape';
 	}
 
 	public function openBlock(string $name): string {
-		return $this->openExpressionTag."block $name".$this->closeExpressionTag;
+		return $this->openExpressionTag . "block $name" . $this->closeExpressionTag;
 	}
 
 	public function closeBlock(): string {
-		return $this->openExpressionTag.'/block'.$this->closeExpressionTag;
+		return $this->openExpressionTag . '/block' . $this->closeExpressionTag;
 	}
 
 	public function asArray(array $array): string {
 		return UArray::asPhpArray_($array);
 	}
 
-	public function insertVariable(string $name,bool $safe=false): string {
-		$filter='';
-		if($safe){
-			$filter='|'.$this->safeFilter;
+	public function insertVariable(string $name, bool $safe = false): string {
+		$filter = '';
+		if ($safe) {
+			$filter = '|' . $this->safeFilter;
 		}
-		$name=\str_replace('.','->',$name);
-		return $this->openVarTag.'$'.$name.$filter.$this->closeVarTag;
+		$name = \str_replace('.', '->', $name);
+		if (UString::contains('(', $name)) {
+			return $this->openVarTag . $name . $filter . $this->closeVarTag;
+		}
+		return $this->openVarTag . '$' . $name . $filter . $this->closeVarTag;
 	}
 
-	public function includeFile(string $filename, bool $asVariable=false): string {
-		$quote="'";
-		if($asVariable){
-			$quote='';
-			$filename='$'.$filename;
+	public function includeFile(string $filename, bool $asVariable = false): string {
+		$quote = "'";
+		if ($asVariable) {
+			$quote = '';
+			$filename = '$' . $filename;
 		}
-		return $this->openExpressionTag."include {$quote}{$filename}{$quote}".$this->closeExpressionTag;
+		return $this->openExpressionTag . "include {$quote}{$filename}{$quote}" . $this->closeExpressionTag;
 	}
 
-	public function extendsTemplate(string $templateName, bool $asVariable=false): string {
-		$quote="'";
-		if($asVariable){
-			$quote='';
-			$templateName='$'.$templateName;
+	public function extendsTemplate(string $templateName, bool $asVariable = false): string {
+		$quote = "'";
+		if ($asVariable) {
+			$quote = '';
+			$templateName = '$' . $templateName;
 		}
-		return $this->openExpressionTag."layout {$quote}{$templateName}{$quote}".$this->closeExpressionTag;
+		return $this->openExpressionTag . "layout {$quote}{$templateName}{$quote}" . $this->closeExpressionTag;
 	}
 
 	public function foreach(string $arrayName, string $value, ?string $key = null): string {
-		$arrayName='$'.$arrayName;
-		$value='$'.$value;
-		if($key!=null){
-			$key='$'.$key;
-			return $this->openExpressionTag."for $arrayName as $key=>$value".$this->closeExpressionTag;
+		$arrayName = '$' . $arrayName;
+		$value = '$' . $value;
+		if ($key != null) {
+			$key = '$' . $key;
+			return $this->openExpressionTag . "for $arrayName as $key=>$value" . $this->closeExpressionTag;
 		}
-		return $this->openExpressionTag."for $arrayName as $value".$this->closeExpressionTag;
+		return $this->openExpressionTag . "for $arrayName as $value" . $this->closeExpressionTag;
 	}
-	
+
 	public function endForeach(): string {
-		return $this->openExpressionTag.'/foreach'.$this->closeExpressionTag;
+		return $this->openExpressionTag . '/foreach' . $this->closeExpressionTag;
 	}
 
 	public function condition(string $condition): string {
-		$condition='$'.\trim($condition);
-		return $this->openExpressionTag."if $condition".$this->closeExpressionTag;
+		$condition = '$' . \trim($condition);
+		return $this->openExpressionTag . "if $condition" . $this->closeExpressionTag;
 	}
 
 	public function endCondition(): string {
-		return $this->openExpressionTag.'/if'.$this->closeExpressionTag;
+		return $this->openExpressionTag . '/if' . $this->closeExpressionTag;
 	}
 
 	public function getNonce(): string {
-		return $this->openExpressionTag."\$nonce??''".$this->closeExpressionTag;
+		return $this->openExpressionTag . "\$nonce??''" . $this->closeExpressionTag;
 	}
-	
+
 	public function getNonceArray(): string {
 		return "['nonce'=>\$nonce??'']";
 	}
 
 	public function getSelf(): string {
-		return $this->openExpressionTag.'$this->getName()'.$this->closeExpressionTag;
+		return $this->openExpressionTag . '$this->getName()' . $this->closeExpressionTag;
 	}
 }
--- src/Ubiquity/views/engine/twig/TemplateParser.php
+++ src/Ubiquity/views/engine/twig/TemplateParser.php
@@ -9,147 +9,147 @@ class TemplateParser {
 
 	private TemplateGenerator $generator;
 
-	private string $varPattern='@\{\{\s?(.*?)\s?\}\}@';
+	private string $varPattern = '@\{\{\s?(.*?)\s?\}\}@';
 
-	private string $blockPattern='@\{\%\s?block\s(.*?)\s?\%\}@';
+	private string $blockPattern = '@\{\%\s?block\s(.*?)\s?\%\}@';
 
 	public function __construct(TemplateGenerator $generator) {
-		$this->generator=$generator;
+		$this->generator = $generator;
 	}
 
 	private function parseVar(string $varStr): array {
-		$parts=\explode('|',$varStr);
-		return ['name'=>trim($parts[0]),'safe'=>isset($parts[1])];
+		$parts = \explode('|', $varStr);
+		return ['name' => trim($parts[0]), 'safe' => isset($parts[1])];
 	}
 
 	private function parseForeach(string $foreachStr): array {
-		$parts=\explode('in',$foreachStr);
-		$kv=explode(',',$parts[0]);
-		$v=\trim($parts[0]);$k=null;
-		if (\count($kv)>1) {
-			$k=\trim($kv[0]);
-			$v=\trim($kv[1]);
+		$parts = \explode('in', $foreachStr);
+		$kv = explode(',', $parts[0]);
+		$v = \trim($parts[0]);
+		$k = null;
+		if (\count($kv) > 1) {
+			$k = \trim($kv[0]);
+			$v = \trim($kv[1]);
 		}
-		return ['array'=>trim($parts[1]),'value'=>$v,'key'=>$k];
+		return ['array' => trim($parts[1]), 'value' => $v, 'key' => $k];
 	}
 
-	private function getExpressionPattern(string $clause){
-		return '@\{\%\s?'.$clause.'\s(.*?)\s?\%\}@';
+	private function getExpressionPattern(string $clause) {
+		return '@\{\%\s?' . $clause . '\s(.*?)\s?\%\}@';
 	}
 
 	protected function parseAllVars(string $text): string {
-		$result=$text;
-		if(\preg_match_all($this->varPattern, $text,$matches)){
-			$originals=$matches[0];
-			$vars=$matches[1];
-			foreach ($vars as $index=>$varStr){
-				$variableInfos=$this->parseVar($varStr);
-				$result=\str_replace($originals[$index], $this->generator->insertVariable($variableInfos['name'], $variableInfos['safe']),$result);
+		$result = $text;
+		if (\preg_match_all($this->varPattern, $text, $matches)) {
+			$originals = $matches[0];
+			$vars = $matches[1];
+			foreach ($vars as $index => $varStr) {
+				$variableInfos = $this->parseVar($varStr);
+				$result = \str_replace($originals[$index], $this->generator->insertVariable($variableInfos['name'], $variableInfos['safe']), $result);
 			}
 		}
 		return $result;
 	}
 
 	protected function parseAllConditions(string $text): string {
-		$result=$text;
-		if(\preg_match_all($this->getExpressionPattern('if'), $text,$matches)){
-			$originals=$matches[0];
-			$conditions=$matches[1];
-			foreach ($conditions as $index=>$cond){
-				$result=\str_replace($originals[$index], $this->generator->condition($cond),$result);
+		$result = $text;
+		if (\preg_match_all($this->getExpressionPattern('if'), $text, $matches)) {
+			$originals = $matches[0];
+			$conditions = $matches[1];
+			foreach ($conditions as $index => $cond) {
+				$result = \str_replace($originals[$index], $this->generator->condition($cond), $result);
 			}
 		}
 		return $result;
 	}
 
 	protected function parseAllForeachs(string $text): string {
-		$result=$text;
-		if(\preg_match_all($this->getExpressionPattern('for'), $text,$matches)){
-			$originals=$matches[0];
-			$foreachs=$matches[1];
-			foreach ($foreachs as $index=>$foreach){
-				$foreachInfos=$this->parseForeach($foreach);
-				$result=\str_replace($originals[$index], $this->generator->foreach($foreachInfos['array'],$foreachInfos['value'],$foreachInfos['key']),$result);
+		$result = $text;
+		if (\preg_match_all($this->getExpressionPattern('for'), $text, $matches)) {
+			$originals = $matches[0];
+			$foreachs = $matches[1];
+			foreach ($foreachs as $index => $foreach) {
+				$foreachInfos = $this->parseForeach($foreach);
+				$result = \str_replace($originals[$index], $this->generator->foreach($foreachInfos['array'], $foreachInfos['value'], $foreachInfos['key']), $result);
 			}
 		}
 		return $result;
 	}
 
 	protected function parseAllBlock(string $text): string {
-		$result=$text;
-		if(\preg_match_all($this->blockPattern, $text,$matches)){
-			$originals=$matches[0];
-			$blocks=$matches[1];
-			foreach ($blocks as $index=>$block){
-				$blockName=\trim($block);
+		$result = $text;
+		if (\preg_match_all($this->blockPattern, $text, $matches)) {
+			$originals = $matches[0];
+			$blocks = $matches[1];
+			foreach ($blocks as $index => $block) {
+				$blockName = \trim($block);
 
-				$result=\str_replace($originals[$index], $this->generator->openBlock($blockName),$result);
+				$result = \str_replace($originals[$index], $this->generator->openBlock($blockName), $result);
 			}
 		}
 		return $result;
 	}
 
-	protected function parseCallback(string $text,string $pattern,$callback): string {
-		$result=$text;
-		if(\preg_match_all($pattern, $text,$matches)){
-			$originals=$matches[0];
-			foreach ($originals as $elm){
-				$result=\str_replace($elm, $callback(),$result);
+	protected function parseCallback(string $text, string $pattern, $callback): string {
+		$result = $text;
+		if (\preg_match_all($pattern, $text, $matches)) {
+			$originals = $matches[0];
+			foreach ($originals as $elm) {
+				$result = \str_replace($elm, $callback(), $result);
 			}
 		}
 		return $result;
 	}
 
-	protected function parseCallbackWithVar(string $text,string $pattern,$callback): string {
-		$result=$text;
-		if(\preg_match_all($pattern, $text,$matches)){
-			$originals=$matches[0];
-			$vars=$matches[1];
-			foreach ($vars as $index=>$elm){
-				$elm=\trim($elm);
-				$result=\str_replace($originals[$index], $callback($elm),$result);
+	protected function parseCallbackWithVar(string $text, string $pattern, $callback): string {
+		$result = $text;
+		if (\preg_match_all($pattern, $text, $matches)) {
+			$originals = $matches[0];
+			$vars = $matches[1];
+			foreach ($vars as $index => $elm) {
+				$elm = \trim($elm);
+				$result = \str_replace($originals[$index], $callback($elm), $result);
 			}
 		}
 		return $result;
 	}
 
-	public function parseFileContent(string $fileContent):string {
-		$gen=$this->generator;
-		$result=$this->parseCallback($fileContent,'@\{\{\s?nonce\s?\}\}@',function(){
+	public function parseFileContent(string $fileContent): string {
+		$result = $this->parseCallback($fileContent, '@\{\{\s?nonce\s?\}\}@', function () {
 			return $this->generator->getNonce();
 		});
-		$result=$this->parseCallback($result,"@\{\s?'nonce':\s?nonce\s?\}@",function(){
+		$result = $this->parseCallback($result, "@\{\s?nonce:\s?nonce\s?\}@", function () {
 			return $this->generator->getNonceArray();
 		});
-		$result=$this->parseCallback($result,'@\{\{\s?_self\s?\}\}@',function(){
+		$result = $this->parseCallback($result, '@\{\{\s?_self\s?\}\}@', function () {
 			return $this->generator->getSelf();
 		});
-		$result=$this->parseAllVars($result);
-		$result=$this->parseAllBlock($result);
-		$result=$this->parseCallback($result,'@\{\%\s?endblock\s?\%\}@',function(){
+		$result = $this->parseAllVars($result);
+		$result = $this->parseAllBlock($result);
+		$result = $this->parseCallback($result, '@\{\%\s?endblock\s?\%\}@', function () {
 			return $this->generator->closeBlock();
 		});
-		$result=$this->parseAllForeachs($result);
-		$result=$this->parseCallback($result,'@\{\%\s?endfor\s?\%\}@',function(){
+		$result = $this->parseAllForeachs($result);
+		$result = $this->parseCallback($result, '@\{\%\s?endfor\s?\%\}@', function () {
 			return $this->generator->endForeach();
 		});
-		$result=$this->parseAllConditions($result);
-		$result=$this->parseCallback($result,'@\{\%\s?endif\s?\%\}@',function(){
+		$result = $this->parseAllConditions($result);
+		$result = $this->parseCallback($result, '@\{\%\s?endif\s?\%\}@', function () {
 			return $this->generator->endCondition();
 		});
-		$result=$this->parseCallbackWithVar($result,'@\{\%\s?extends\s?(.*?)\s?\%\}@',function($name){
-			$asVariable=!UString::containsValues(['"',"'"],$name);
-			return $this->generator->extendsTemplate($name,$asVariable);
+		$result = $this->parseCallbackWithVar($result, '@\{\%\s?extends\s?(.*?)\s?\%\}@', function ($name) {
+			$asVariable = !UString::containsValues(['"', "'"], $name);
+			return $this->generator->extendsTemplate($name, $asVariable);
 		});
-		$result=$this->parseCallbackWithVar($result,'@\{\%\s?include\s?(.*?)\s?\%\}@',function($name){
-			$asVariable=!UString::containsValues(['"',"'"],$name);
-			return $this->generator->includeFile($name,$asVariable);
+		$result = $this->parseCallbackWithVar($result, '@\{\%\s?include\s?(.*?)\s?\%\}@', function ($name) {
+			$asVariable = !UString::containsValues(['"', "'"], $name);
+			return $this->generator->includeFile($name, $asVariable);
 		});
-		$result=$this->parseCallbackWithVar($result,'@\{\{\s(.*?)\s?\}\}@',function($content){
-			return $this->generator->getOpenVarTag().$content.$this->generator->getCloseVarTag();
+		$result = $this->parseCallbackWithVar($result, '@\{\{\s(.*?)\s?\}\}@', function ($content) {
+			return $this->generator->getOpenVarTag() . $content . $this->generator->getCloseVarTag();
 		});
-		$result=$this->parseCallbackWithVar($result,'@\{\%\s(.*?)\s?\%\}@',function($content){
-			return $this->generator->getOpenExpressionTag().$content.$this->generator->getCloseExpressionTag();
+		$result = $this->parseCallbackWithVar($result, '@\{\%\s(.*?)\s?\%\}@', function ($content) {
+			return $this->generator->getOpenExpressionTag() . $content . $this->generator->getCloseExpressionTag();
 		});
 		return $result;
 	}
