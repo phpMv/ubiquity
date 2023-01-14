0292bd18c2103bef7eabb81c07d18da957a11f18
jcheron <myaddressmail@gmail.com>
2023-01-14 02:54:43 +0100

    [skip ci] update UString

--- src/Ubiquity/utils/base/UString.php
+++ src/Ubiquity/utils/base/UString.php
@@ -9,22 +9,22 @@ namespace Ubiquity\utils\base;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.5
+ * @version 1.0.6
  *
  */
 class UString {
 
 	public static function startswith($hay, $needle) {
-		return \substr ( $hay, 0, strlen ( $needle ) ) === $needle;
+		return \substr($hay, 0, strlen($needle)) === $needle;
 	}
 
 	public static function contains($needle, $haystack) {
-		return \strpos ( $haystack, $needle ) !== false;
+		return \strpos($haystack, $needle) !== false;
 	}
-	
-	public static function containsValues(array $values,string $haystack):bool{
-		foreach ($values as $v){
-			if(\strpos ( $haystack, $v ) !== false){
+
+	public static function containsValues(array $values, string $haystack): bool {
+		foreach ($values as $v) {
+			if (\strpos($haystack, $v) !== false) {
 				return true;
 			}
 		}
@@ -32,7 +32,7 @@ class UString {
 	}
 
 	public static function endswith($hay, $needle) {
-		return \substr ( $hay, - strlen ( $needle ) ) === $needle;
+		return \substr($hay, -strlen($needle)) === $needle;
 	}
 
 	public static function getBooleanStr($value) {
@@ -40,27 +40,27 @@ class UString {
 	}
 
 	public static function isNull($s) {
-		return (! isset ( $s ) || NULL === $s || '' === $s);
+		return (!isset ($s) || null === $s || '' === $s);
 	}
 
 	public static function isNotNull($s) {
-		return (isset ( $s ) && NULL !== $s && '' !== $s);
+		return (isset ($s) && null !== $s && '' !== $s);
 	}
 
 	public static function isBooleanTrue($s) {
-		return filter_var ( $s, FILTER_VALIDATE_BOOLEAN ) === true;
+		return filter_var($s, FILTER_VALIDATE_BOOLEAN) === true;
 	}
 
 	public static function isBooleanFalse($s) {
-		return \filter_var ( $s, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE ) === false;
+		return \filter_var($s, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) === false;
 	}
 
 	public static function isBoolean($value) {
-		return \is_bool ( $value );
+		return \is_bool($value);
 	}
 
 	public static function isBooleanStr($value) {
-		return filter_var ( $value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE ) !== NULL;
+		return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) !== null;
 	}
 
 	/**
@@ -79,54 +79,54 @@ class UString {
 		} elseif ($count === 1) {
 			$result = $one;
 		}
-		return \str_replace ( '{count}', $count, $result );
+		return \str_replace('{count}', $count, $result);
 	}
 
 	public static function firstReplace($haystack, $needle, $replace) {
 		$newstring = $haystack;
-		$pos = \strpos ( $haystack, $needle );
+		$pos = \strpos($haystack, $needle);
 		if ($pos !== false) {
-			$newstring = \substr_replace ( $haystack, $replace, $pos, \strlen ( $needle ) );
+			$newstring = \substr_replace($haystack, $replace, $pos, \strlen($needle));
 		}
 		return $newstring;
 	}
 
 	public static function replaceFirstOccurrence($pattern, $replacement, $subject) {
-		$pattern = '/' . \preg_quote ( $pattern, '/' ) . '/';
-		return \preg_replace ( $pattern, $replacement, $subject, 1 );
+		$pattern = '/' . \preg_quote($pattern, '/') . '/';
+		return \preg_replace($pattern, $replacement, $subject, 1);
 	}
 
 	public static function replaceArray($haystack, $needleArray, $replace) {
 		$result = $haystack;
-		foreach ( $needleArray as $needle ) {
-			$result = self::firstReplace ( $result, $needle, $replace );
+		foreach ($needleArray as $needle) {
+			$result = self::firstReplace($result, $needle, $replace);
 		}
 		return $result;
 	}
 
 	public static function doubleBackSlashes($value) {
-		if (\is_string ( $value ))
-			return \str_replace ( "\\", "\\\\", $value );
+		if (\is_string($value))
+			return \str_replace("\\", "\\\\", $value);
 		return $value;
 	}
 
 	public static function cleanAttribute($attr, $replacement = "-") {
-		$attr = \preg_replace ( '/[^a-zA-Z0-9\-]/s', $replacement, $attr );
-		while ( $attr !== ($attr = \str_replace ( $replacement . $replacement, $replacement, $attr )) )
+		$attr = \preg_replace('/[^a-zA-Z0-9\-]/s', $replacement, $attr);
+		while ($attr !== ($attr = \str_replace($replacement . $replacement, $replacement, $attr)))
 			;
 		return $attr;
 	}
 
 	public static function mask($secretString, $maskChar = "*") {
-		return \str_repeat ( $maskChar, \strlen ( $secretString ) );
+		return \str_repeat($maskChar, \strlen($secretString));
 	}
 
 	public static function isValid($value) {
-		return \is_scalar ( $value ) || (\is_object ( $value ) && \method_exists ( $value, '__toString' ));
+		return \is_scalar($value) || (\is_object($value) && \method_exists($value, '__toString'));
 	}
 
 	public static function isJson($value) {
-		return \is_object ( \json_decode ( $value ) );
+		return \is_object(\json_decode($value));
 	}
 
 	/**
@@ -136,7 +136,7 @@ class UString {
 	 * @return string
 	 */
 	public static function toString($value) {
-		if (self::isValid ( $value )) {
+		if (self::isValid($value)) {
 			return $value . '';
 		}
 		return '';
@@ -150,7 +150,18 @@ class UString {
 	 * @return array
 	 */
 	public static function explode($delimiters, $string) {
-		return \explode ( $delimiters [0], \str_replace ( $delimiters, $delimiters [0], $string ) );
+		return \explode($delimiters [0], \str_replace($delimiters, $delimiters [0], $string));
+	}
+
+	/**
+	 * Returns true is value is a php expression.
+	 * @param string $v
+	 * @return bool
+	 */
+	public static function isExpression(string $v): bool {
+		$v = \trim($v);
+		return $v == 'nonce' || self::startswith($v, '$') || self::startswith($v, 'function')
+			|| self::startswith($v, 'array(') || self::startswith($v, 'getenv(');
 	}
 }
 
