4457daf2c030ace6b3f3a436b0808d094c72941f
jcheron <myaddressmail@gmail.com>
2023-01-15 01:27:39 +0100

    Update LatteTemplateGenerator.php

--- src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
+++ src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
@@ -4,15 +4,15 @@ namespace Ubiquity\views\engine\latte;
 
 class LatteTemplateGenerator extends \Ubiquity\views\engine\TemplateGenerator {
 
-	private function replaceSelf(string $code): string {
-		return \str_replace('$_self','$this->getName()', $code);
+	private function replaceSpe(string $code): string {
+		return \str_replace(['$_self', '$nonce'], ['$this->getName()', '$nonce??""'], $code);
 	}
-	
+
 	public function parseFromTwig(string $code) {
 		if (\class_exists(\LatteTools\TwigConverter::class)) {
 			$converter = new \LatteTools\TwigConverter();
-			$code=$converter->convert($code);
-			return $this->replaceSelf($code);
+			$code = $converter->convert($code);
+			return $this->replaceSpe($code);
 		}
 		return '\LatteTools\TwigConverter does not exist!';
 	}
