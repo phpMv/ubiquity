ed3d739a0a4f4ec75e3fa02ba57d24e1a2b49977
jcheron <myaddressmail@gmail.com>
2023-01-15 03:56:00 +0100

    [skip ci] fix isExpired

--- src/Ubiquity/views/engine/latte/ULatteFileLoader.php
+++ src/Ubiquity/views/engine/latte/ULatteFileLoader.php
@@ -4,25 +4,31 @@ namespace Ubiquity\views\engine\latte;
 
 class ULatteFileLoader extends \Latte\Loaders\FileLoader {
 
-	private array $namespaces=[];
+	private array $namespaces = [];
 
 	private function getFilename(string $fileName): string {
 		if (\preg_match('/\@(.*?)\//', $fileName, $match) == 1) {
-			$ns='@'.$match[1];
-			if ($dir=$this->namespaces[$ns]??false) {
-				return \realpath(\str_replace($ns,$dir,$fileName));
+			$ns = '@' . $match[1];
+			if ($dir = $this->namespaces[$ns] ?? false) {
+				return \realpath(\str_replace($ns, $dir, $fileName));
 			}
 		}
-		return $this->baseDir.$fileName;
+		return $this->baseDir . $fileName;
 	}
 
 	public function addPath($path, $namespace): void {
-		$this->namespaces['@'.\ltrim($namespace,'@')]=$path;
+		$this->namespaces['@' . \ltrim($namespace, '@')] = $path;
 	}
 
 	public function getContent(string $fileName): string {
-		$file=$this->getFilename($fileName);
+		$file = $this->getFilename($fileName);
 		return \file_get_contents($file);
+	}
+
+	public function isExpired(string $file, int $time): bool {
+		$file = $this->getFilename($file);
+		$mtime = @filemtime($file); // @ - stat may fail
+		return !$mtime || $mtime > $time;
 	}
 
 	/**
