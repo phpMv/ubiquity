40069b9272a0a28d9c3ae2f05c91ad22cf7bc34d
jcheron <myaddressmail@gmail.com>
2023-03-16 13:10:40 +0100

    [content][skip ci] add HidePassword transformer

--- src/Ubiquity/contents/transformation/TransformersManager.php
+++ src/Ubiquity/contents/transformation/TransformersManager.php
@@ -3,16 +3,16 @@
 namespace Ubiquity\contents\transformation;
 
 use Ubiquity\cache\CacheManager;
+use Ubiquity\contents\transformation\transformers\Boolean;
 use Ubiquity\contents\transformation\transformers\DateTime;
 use Ubiquity\contents\transformation\transformers\FirstUpperCase;
+use Ubiquity\contents\transformation\transformers\HidePassword;
 use Ubiquity\contents\transformation\transformers\LowerCase;
 use Ubiquity\contents\transformation\transformers\Md5;
 use Ubiquity\contents\transformation\transformers\Password;
 use Ubiquity\contents\transformation\transformers\UpperCase;
 use Ubiquity\orm\DAO;
 use Ubiquity\orm\OrmUtils;
-use Ubiquity\utils\base\UArray;
-use Ubiquity\contents\transformation\transformers\Boolean;
 
 /**
  * Transform objects after loading
@@ -25,12 +25,12 @@ use Ubiquity\contents\transformation\transformers\Boolean;
  *
  */
 class TransformersManager {
-	const TRANSFORMER_TYPES = [ 'reverse' => TransformerInterface::class,'transform' => TransformerInterface::class,'toView' => TransformerViewInterface::class,'toForm' => TransformerFormInterface::class ];
+	const TRANSFORMER_TYPES = ['reverse' => TransformerInterface::class, 'transform' => TransformerInterface::class, 'toView' => TransformerViewInterface::class, 'toForm' => TransformerFormInterface::class];
 	/**
 	 *
 	 * @var array|mixed
 	 */
-	private static $transformers = [ 'md5' => Md5::class,'datetime' => DateTime::class,'upper' => UpperCase::class,'firstUpper' => FirstUpperCase::class,'lower' => LowerCase::class,'password' => Password::class,'boolean'=>Boolean::class ];
+	private static $transformers = ['md5' => Md5::class, 'datetime' => DateTime::class, 'upper' => UpperCase::class, 'firstUpper' => FirstUpperCase::class, 'lower' => LowerCase::class, 'password' => Password::class, 'hpassword' => HidePassword::class, 'boolean' => Boolean::class];
 	private static $key = 'contents/transformers';
 
 	/**
@@ -38,10 +38,10 @@ class TransformersManager {
 	 * Do not use at runtime !
 	 */
 	public static function start() {
-		if (CacheManager::$cache->exists ( self::$key )) {
-			self::$transformers = \array_merge ( self::$transformers, CacheManager::$cache->fetch ( self::$key ) );
+		if (CacheManager::$cache->exists(self::$key)) {
+			self::$transformers = \array_merge(self::$transformers, CacheManager::$cache->fetch(self::$key));
 		}
-		if (\class_exists ( '\\Ubiquity\\security\\csrf\\CsrfManager' )) {
+		if (\class_exists('\\Ubiquity\\security\\csrf\\CsrfManager')) {
 			self::$transformers ['crypt'] = '\\Ubiquity\\contents\\transformation\\transformers\\Crypt';
 		}
 	}
@@ -75,9 +75,9 @@ class TransformersManager {
 	 * @param string $classname
 	 */
 	public static function registerClassAndSave(string $transformer, string $classname): void {
-		self::start ();
-		self::registerClass ( $transformer, $classname );
-		self::store ();
+		self::start();
+		self::registerClass($transformer, $classname);
+		self::store();
 	}
 
 	/**
@@ -87,8 +87,8 @@ class TransformersManager {
 	 * @param array $transformersAndClasses
 	 */
 	public static function registerClasses(array $transformersAndClasses): void {
-		foreach ( $transformersAndClasses as $transformer => $class ) {
-			self::registerClass ( $transformer, $class );
+		foreach ($transformersAndClasses as $transformer => $class) {
+			self::registerClass($transformer, $class);
 		}
 	}
 
@@ -99,11 +99,11 @@ class TransformersManager {
 	 * @param array $transformersAndClasses
 	 */
 	public static function registerClassesAndSave(array $transformersAndClasses): void {
-		self::start ();
-		foreach ( $transformersAndClasses as $transformer => $class ) {
-			self::registerClass ( $transformer, $class );
+		self::start();
+		foreach ($transformersAndClasses as $transformer => $class) {
+			self::registerClass($transformer, $class);
 		}
-		self::store ();
+		self::store();
 	}
 
 	/**
@@ -113,7 +113,7 @@ class TransformersManager {
 	 * @return ?string
 	 */
 	public static function getTransformerClass(string $transformer): ?string {
-		if (isset ( self::$transformers [$transformer] )) {
+		if (isset (self::$transformers [$transformer])) {
 			return self::$transformers [$transformer];
 		}
 		return null;
@@ -129,8 +129,8 @@ class TransformersManager {
 	 */
 	public static function transform(object $instance, string $member, ?string $transform = 'transform') {
 		$getter = 'get' . $member;
-		if (\method_exists ( $instance, $getter )) {
-			return self::applyTransformer ( $instance, $member, $instance->{$getter} (), $transform );
+		if (\method_exists($instance, $getter)) {
+			return self::applyTransformer($instance, $member, $instance->{$getter} (), $transform);
 		}
 		return null;
 	}
@@ -145,11 +145,11 @@ class TransformersManager {
 	 * @return mixed
 	 */
 	public static function applyTransformer(object $instance, string $member, $value, ?string $transform = 'transform') {
-		$class = \get_class ( $instance );
-		$metas = OrmUtils::getModelMetadata ( $class );
-		if (isset ( $metas ['#transformers'] [$transform] [$member] )) {
+		$class = \get_class($instance);
+		$metas = OrmUtils::getModelMetadata($class);
+		if (isset ($metas ['#transformers'] [$transform] [$member])) {
 			$transformer = $metas ['#transformers'] [$transform] [$member];
-			return $transformer::$transform ( $value );
+			return $transformer::$transform ($value);
 		}
 		return $value;
 	}
@@ -161,16 +161,16 @@ class TransformersManager {
 	 * @param string $transform
 	 */
 	public static function transformInstance(object $instance, $transform = 'transform'): void {
-		$class = \get_class ( $instance );
-		$metas = OrmUtils::getModelMetadata ( $class );
-		$transformers = $metas ['#transformers'] [$transform] ?? [ ];
-		foreach ( $transformers as $member => $transformer ) {
-			$getter = 'get' . ucfirst ( $member );
-			$setter = 'set' . ucfirst ( $member );
-			if (\method_exists ( $instance, $getter )) {
-				$value = $transformer::$transform ( $instance->{$getter} () );
-				if (\method_exists ( $instance, $setter )) {
-					$instance->{$setter} ( $value );
+		$class = \get_class($instance);
+		$metas = OrmUtils::getModelMetadata($class);
+		$transformers = $metas ['#transformers'] [$transform] ?? [];
+		foreach ($transformers as $member => $transformer) {
+			$getter = 'get' . ucfirst($member);
+			$setter = 'set' . ucfirst($member);
+			if (\method_exists($instance, $getter)) {
+				$value = $transformer::$transform ($instance->{$getter} ());
+				if (\method_exists($instance, $setter)) {
+					$instance->{$setter} ($value);
 				}
 				$instance->_rest [$member] = $value;
 			}
@@ -181,7 +181,7 @@ class TransformersManager {
 	 * Store the loaded transformers in cache.
 	 */
 	public static function store(): void {
-		CacheManager::$cache->store ( self::$key, self::$transformers );
+		CacheManager::$cache->store(self::$key, self::$transformers);
 	}
 }
 
---
+++ src/Ubiquity/contents/transformation/transformers/HidePassword.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ubiquity\contents\transformation\transformers;
+
+use Ubiquity\contents\transformation\TransformerViewInterface;
+
+/**
+ * Hide a password.
+ * Ubiquity\contents\transformation\transformers$HidePassword
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.0
+ * @since 2.4.6
+ */
+class HidePassword implements TransformerViewInterface {
+
+	public static function toView($value) {
+		return '';
+	}
+}
