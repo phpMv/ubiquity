69035e7ccd65cf4415523fc7853934bd1325c29a
J-C HERON <myaddressmail@gmail.com>
2023-03-24 17:11:50 +0100

    [skip ci][events] update listener methods

--- src/Ubiquity/events/EventsManager.php
+++ src/Ubiquity/events/EventsManager.php
@@ -6,69 +6,92 @@ use Ubiquity\cache\CacheManager;
 /**
  * Manage events
  *
- * @author jc
+ * Ubiquity\contents\transformation\events$EventsManager
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.2
  *
  */
 class EventsManager {
 
-	private static string $key = 'events/events';
+    private static string $key = 'events/events';
 
-	/**
-	 *
-	 * @var array|mixed
-	 */
-	protected static $managedEvents = [];
+    /**
+     *
+     * @var array|mixed
+     */
+    protected static $managedEvents = [];
 
-	/**
-	 * Starts the event manager (in app/config/services.php)
-	 */
-	public static function start():void {
-		if (CacheManager::$cache->exists(self::$key)) {
-			self::$managedEvents = CacheManager::$cache->fetch(self::$key);
-		}
-	}
+    public static function addOneListener(string $eventName, $action):void {
+        self::$managedEvents[$eventName] ??= [];
+        self::$managedEvents[$eventName][] = $action;
+    }
 
-	/**
-	 * Adds a listener on eventName
-	 * @param string $eventName
-	 * @param EventListenerInterface|callable $action
-	 */
-	public static function addListener(string $eventName, $action):void {
-		if (! isset(self::$managedEvents[$eventName])) {
-			self::$managedEvents[$eventName] = [];
-		}
-		self::$managedEvents[$eventName][] = $action;
-	}
+    /**
+     * Starts the event manager (in app/config/services.php)
+     */
+    public static function start():void {
+        if (CacheManager::$cache->exists(self::$key)) {
+            self::$managedEvents = CacheManager::$cache->fetch(self::$key);
+        }
+    }
 
-	/**
-	 * Store the managed events in cache (do not use in prod)
-	 */
-	public static function store():void {
-		CacheManager::$cache->store(self::$key, self::$managedEvents);
-	}
 
-	/**
-	 * Trigger an event
-	 * @param string $eventName
-	 * @param mixed ...$params
-	 */
-	public static function trigger(string $eventName, &...$params):void {
-		if (isset(self::$managedEvents[$eventName])) {
-			foreach (self::$managedEvents[$eventName] as $action) {
-				self::triggerOne($action, $params);
-			}
-		}
-	}
+    /**
+     * Adds a listener on eventName.
+     * @param string|array $eventNames
+     * @param EventListenerInterface|callable $action
+     */
+    public static function addListener($eventNames, $action):void {
+        if (\is_array($eventNames)) {
+            foreach ($eventNames as $eventName) {
+                self::addOneListener($eventName, $action);
+            }
+        } else {
+            self::addOneListener($eventNames, $action);
+        }
+    }
 
-	private static function triggerOne($action, &$params):void {
-		if (\is_callable($action)) {
-			\call_user_func_array($action, $params);
-		} elseif (is_subclass_of($action, EventListenerInterface::class)) {
-			\call_user_func_array([
-				new $action(),
-				'on'
-			], $params);
-		}
-	}
+    /**
+     * Adds a list of listeners
+     * @param array $listeners
+     */
+    public static function addListeners(array $listeners):void {
+        foreach ($listeners as $eventName => $action) {
+            self::addOneListener($eventName, $action);
+        }
+    }
+
+    /**
+     * Store the managed events in cache (do not use in prod)
+     */
+    public static function store():void {
+        CacheManager::$cache->store(self::$key, self::$managedEvents);
+    }
+
+    /**
+     * Trigger an event
+     * @param string $eventName
+     * @param mixed ...$params
+     */
+    public static function trigger(string $eventName, &...$params):void {
+        if (isset(self::$managedEvents[$eventName])) {
+            foreach (self::$managedEvents[$eventName] as $action) {
+                self::triggerOne($action, $params);
+            }
+        }
+    }
+
+    private static function triggerOne($action, &$params):void {
+        if (\is_callable($action)) {
+            \call_user_func_array($action, $params);
+        } elseif (is_subclass_of($action, EventListenerInterface::class)) {
+            \call_user_func_array([
+                new $action(),
+                'on'
+            ], $params);
+        }
+    }
 }
 
